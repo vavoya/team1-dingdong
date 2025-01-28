0f7b235bb975eea1130f0d7ac02024fe9ba5b317
admin <admin@admins-MacBook-Pro-4.local>
2025-01-29 00:57:12 +0900

    fix: 프로젝트 초기 셋팅 build 에러 문제 트러블 슈팅. - tsconfig.app.json의 baseUrl, paths 설정이 이미 존재하는데 tsconfig.json에서도 옵션을 사용하려는 부분에서 생긴 오류 -> 제거 완료. - index.html에서 js파일 포함 script 태그가 빠짐 원인 -> 추가완료. - vite.config.js에서 alias 설정에서 /src 폴더 설정 ./src -> /src로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: './src' },  // `@`을 `src` 폴더로 매핑
-    ]
+      { find: "@", replacement: "/src" }, // `@`을 `src` 폴더로 매핑
+    ],
   },
-})
+});
