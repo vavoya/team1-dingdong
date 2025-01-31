b9d9cf07b800383182d902205bcd5d9db4c94ea8
popopy0412 <philip000412@gmail.com>
2025-01-31 17:12:57 +0900

    [feat]: DB에 동적으로 위치 추가 및 삭제 API 구현

--- backend/src/main/java/com/ddbb/dingdong/domain/clustering/controller/LocationController.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/clustering/controller/LocationController.java
@@ -2,12 +2,12 @@ package com.ddbb.dingdong.domain.clustering.controller;
 
 import com.ddbb.dingdong.domain.clustering.repository.LocationRepository;
 import com.ddbb.dingdong.domain.clustering.entity.Location;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/locations")
 public class LocationController {
 
     private final LocationRepository locationRepository;
@@ -16,8 +16,18 @@ public class LocationController {
         this.locationRepository = locationRepository;
     }
 
-    @GetMapping("/locations")
+    @GetMapping
     public List<Location> getAllLocations() {
         return locationRepository.findAll();
+    }
+
+    @PostMapping
+    public Location addLocation(@RequestBody Location location) {
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping
+    public void deleteAllLocations() {
+        locationRepository.deleteAll();
     }
 }
