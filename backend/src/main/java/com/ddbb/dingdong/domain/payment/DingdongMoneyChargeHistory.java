3bd50070c4952228ce3f48bc574de58b86caf170
keemjoonsung <kjs990114@naver.com>
2025-01-31 15:01:22 +0900

    [feat] JPA Entity 구현 - DingdongMoneyChargeHistory.java
    실제 결제 API에서 반환하는 값을 저장하고 있는 로그 테이블인 DingdongMoneyChargeHistory.java를 구현
    
    #9

--- backend/src/main/java/com/ddbb/dingdong/domain/payment/DingdongMoneyChargeHistory.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/payment/DingdongMoneyChargeHistory.java
@@ -1,4 +1,35 @@
 package com.ddbb.dingdong.domain.payment;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DingdongMoneyChargeHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String orderName;
+
+    @Column(nullable = false)
+    private String method;
+
+    @Column(nullable = false)
+    private Integer totalAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime requestedAt;
+
+    private LocalDateTime approvedAt;
 }
