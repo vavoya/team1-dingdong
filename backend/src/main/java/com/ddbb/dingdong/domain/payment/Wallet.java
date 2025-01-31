2967a3452fdae222714f24c3130dc79a4c4d2601
keemjoonsung <kjs990114@naver.com>
2025-01-31 15:02:34 +0900

    [feat] JPA Entity 구현 - Wallet.java
    딩동머니의 잔고를 관리하는 Wallet 테이블 구현
    
    #9

--- backend/src/main/java/com/ddbb/dingdong/domain/payment/Wallet.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/payment/Wallet.java
@@ -1,4 +1,27 @@
 package com.ddbb.dingdong.domain.payment;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Integer balance;
+
+    @Column(nullable = false)
+    private LocalDateTime lastUpdatedAt;
 }
