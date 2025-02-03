babdb7903654e79a393ad3601127d3bab18fec58
ji-woong-song <thdwldnd7@naver.com>
2025-02-03 15:50:54 +0900

    [feat]: BusSimulator - Supplier<Point>를 기능 추가
    softeer5th#58

--- backend/src/main/java/com/ddbb/dingdong/simulator/subscription/source/BusSimulator.java
+++ backend/src/main/java/com/ddbb/dingdong/simulator/subscription/source/BusSimulator.java
@@ -3,12 +3,21 @@ package com.ddbb.dingdong.simulator.subscription.source;
 import org.springframework.data.geo.Point;
 
 import java.util.List;
+import java.util.function.Supplier;
 
-public class BusSimulator{
+public class BusSimulator implements Supplier<Point> {
     private final List<Point> simulatedPoints;
     private int currentPosition = 0;
 
     public BusSimulator(List<Point> simulatedPoints) {
         this.simulatedPoints = simulatedPoints;
+    }
+
+    @Override
+    public Point get() {
+        if (currentPosition >= simulatedPoints.size()) {
+            return null;
+        }
+        return simulatedPoints.get(currentPosition++);
     }
 }
