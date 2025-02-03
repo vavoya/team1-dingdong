faa922264858b5e0202e98ddeefa9b4b8d7b5c4d
vavoya <vavoya6324@gmail.com>
2025-02-03 14:51:12 +0900

    [style]: Reservations 페이지 스타일 추가 - BookingActionButton, CardSlider 스타일 파일 추가

---
+++ src/pages/Reservations/components/BookingActionButton/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import {Body2Medium} from "@/styles/typography.ts";
+import {colors} from "@/styles/colors.ts";
+
+
+export const ActionButton = styled.button`
+    display: flex;
+    height: 36px;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 4px;
+    border: 1px solid ${colors.gray30};
+    background: ${colors.gray0};
+
+
+    &:disabled {
+        background: ${colors.gray30};
+    }
+`
+export const ActionText = styled(Body2Medium).withConfig({
+    shouldForwardProp: (prop) => prop !== "disabled"
+})<{disabled: boolean}>`
+    color: ${({disabled}) => disabled ? colors.gray0 : colors.gray100};
+
+    /* Body 2/Medium */
+    font-family: Pretendard,serif;
+    font-style: normal;
+`
