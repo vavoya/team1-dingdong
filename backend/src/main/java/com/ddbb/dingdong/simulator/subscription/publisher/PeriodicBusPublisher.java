38e987374b72a421ff6ac4bc956196db5013a336
ji-woong-song <thdwldnd7@naver.com>
2025-02-03 18:23:00 +0900

    [feat]: Publisher를 생성하는 BusPublisherFactory 추가
    실제 버스 GPS를 받아오는 구현체에 대한 생성 로직을 추상화 하기 위함.
    
    softeer5th#58

--- backend/src/main/java/com/ddbb/dingdong/simulator/subscription/publisher/PeriodicBusPublisher.java
+++ backend/src/main/java/com/ddbb/dingdong/simulator/subscription/publisher/PeriodicBusPublisher.java
@@ -11,7 +11,7 @@ public class PeriodicBusPublisher<T> extends SubmissionPublisher<T> {
     private final ScheduledExecutorService scheduler;
     private final ScheduledFuture<?> periodicTask;
 
-    public PeriodicBusPublisher(
+    PeriodicBusPublisher(
             BusSubscriptionManager manager, long busId,
             Supplier<T> supplier, long period, long initialDelay, TimeUnit unit
     ) {
