9c9f859391e9ca4851fbaf823df5650170384dd1
vavoya <vavoya6324@gmail.com>
2025-02-03 14:54:48 +0900

    [feat]: 실시간 버스 위치 페이지 구현

---
+++ src/pages/BusTracker/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+
+export const PageWrapper = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+`
+
+export const BusCardSection = styled.section`
+    position: absolute;
+    z-index: 1;
+    text-align: right;
+    left: 20px;
+    right: 20px;
+    bottom: 20px;
+`
