fb0bf48e81021714beabc5b59d88d7017a67c4f4
vavoya <vavoya6324@gmail.com>
2025-02-04 14:37:28 +0900

    feat: BusState 컴포넌트 및 스타일, 기능 추가
    - 버스 상태 정보를 표시하기 위한 BusState 컴포넌트 도입
    - styled-components를 활용한 스타일 및 레이아웃 구성
    - `type` prop에 따라 BusStateCard 또는 BusReservation을 조건부 렌더링하도록 구현
    - 네비게이션을 위한 인터랙티브 헤더 통합

---
+++ src/pages/Home/component/BusState/styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import {colors} from "@/styles/colors.ts";
+
+export const Wrapper = styled.section`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background: ${colors.gray0};
+`
+export const Header = styled.header`
+    display: flex;
+    padding: 24px 20px 10px 20px;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+`
+export const HeaderLeftSection = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 4px;
+`
+export const HeaderTitle = styled.span`
+    color: ${colors.gray100};
+    font-family: Pretendard, serif;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 150%;
+`
+export const HeaderRightSection = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 4px;
+`
+export const HeaderInfoText = styled.span`
+    color: ${colors.gray70};
+    font-family: Pretendard, serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 150%;
+`
+export const HeaderActionButton = styled.button`
+    display: flex;
+    width: 14px;
+    height: 24px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`
+export const CardList = styled.ul`
+    display: flex;
+    padding: 0 20px;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    align-self: stretch;
+`
