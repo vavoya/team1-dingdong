38af895856c7b3db6bb98e3233e9b6e1b7953d08
vavoya <vavoya6324@gmail.com>
2025-02-05 14:39:20 +0900

    feat: 기본 이미지 아이콘(DefaultImgIcon) 컴포넌트 추가
    - 디자인 시스템에 기본 SVG 아이콘을 제공하는 DefaultImgIcon 컴포넌트를 추가
    - 이 컴포넌트는 TimeTableManagement 섹션에서 활용될 예정

---
+++ src/components/designSystem/Icons/TimeTableManagement/DefaultImgIcon.tsx
@@ -0,0 +1,12 @@
+
+
+export default function DefaultImgIcon() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+            <rect width="50" height="50" rx="4" fill="#D7D7D7"/>
+            <rect x="0.5" y="0.5" width="49" height="49" rx="3.5" stroke="black" strokeOpacity="0.1"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M17.625 15C16.1753 15 15 16.1513 15 17.5714V30.4286C15 31.8487 16.1753 33 17.625 33H33.375C34.8247 33 36 31.8487 36 30.4286V17.5714C36 16.1513 34.8247 15 33.375 15H17.625ZM33.375 30.4286H17.625L22.875 20.1429L26.8125 27.8571L29.4375 22.7143L33.375 30.4286Z" fill="black" fillOpacity="0.2"/>
+        </svg>
+    )
+}
