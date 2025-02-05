24a01f1f4fbbfd0dac49841b16ac221949fc3156
popopy0412 <philip000412@gmail.com>
2025-02-05 16:59:45 +0900

    [fix]: 사용하지 않는 클러스터링 알고리즘 관련 파일 및 로직 삭제

--- backend/src/main/java/com/ddbb/dingdong/domain/clustering/controller/ClusterController.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/clustering/controller/ClusterController.java
@@ -2,7 +2,7 @@ package com.ddbb.dingdong.domain.clustering.controller;
 
 import com.ddbb.dingdong.domain.clustering.entity.Location;
 import com.ddbb.dingdong.domain.clustering.service.ElkiClusteringService;
-import com.ddbb.dingdong.domain.clustering.service.SmileClusteringService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,31 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/cluster")
 public class ClusterController {
 
-    private final SmileClusteringService smileClusteringService;
     private final ElkiClusteringService elkiClusteringService;
-
-    public ClusterController(
-            SmileClusteringService smileClusteringService,
-            ElkiClusteringService elkiClusteringService
-    ) {
-        this.smileClusteringService = smileClusteringService;
-        this.elkiClusteringService = elkiClusteringService;
-    }
-
-    @GetMapping("/smile/kmeans")
-    public String smileKmeans(@RequestParam(defaultValue = "3") int k) {
-        smileClusteringService.smileKmeans(k);
-        return "Smile - K-Means 클러스터링 완료! 클러스터 수 = " + k;
-    }
-
-    @GetMapping("/smile/dbscan")
-    public String smileDBScan(@RequestParam(defaultValue = "3") double radius, @RequestParam(defaultValue = "2") int minPts) {
-        smileClusteringService.smileDBScan(radius, minPts);
-        return "Smile - DBSCAN 클러스터링 완료! 반경 = " + radius + "km" + ", 최소 점 갯수 = " + minPts;
-    }
 
     @GetMapping("/elki/kmeans")
     public String elkiKmeans(@RequestParam(defaultValue = "3") int k) {
