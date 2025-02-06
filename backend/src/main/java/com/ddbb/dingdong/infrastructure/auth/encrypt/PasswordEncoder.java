cc1b893cb5409d61c919a11aeab7ab2705feab54
keemjoonsung <kjs990114@naver.com>
2025-02-06 17:13:13 +0900

    [fix] PasswordEncoder와 SHA512 알고리즘 분리
    SHA512를 제공하는 객체와 PasswordEncoder를 분리시켜, PasswordEncoder에서 SHA512를 주입받게끔 의존성 분리
    
    #107

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/encrypt/PasswordEncoder.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/encrypt/PasswordEncoder.java
@@ -1,19 +1,17 @@
 package com.ddbb.dingdong.infrastructure.auth.encrypt;
 
+import com.ddbb.dingdong.infrastructure.auth.encrypt.utils.SHA512Encoder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
 @Component
-public class SHA512PasswordEncoder {
-    private static final String HASH_ALGORITHM = "SHA-512";
+@RequiredArgsConstructor
+public class PasswordEncoder {
     private static final int HASH_ITERATIONS = 12;
     private static final int SALT_LENGTH = 16;
-
+    private final SHA512Encoder encoder;
     public String encode(String rawPassword) {
         byte[] salt = generateSalt();
         return encodePassword(rawPassword, salt, HASH_ITERATIONS);
@@ -21,7 +19,9 @@ public class SHA512PasswordEncoder {
 
     public boolean matches(String rawPassword, String encryptedPassword) {
         String[] parts = encryptedPassword.split("\\$");
-        if(parts.length != 3) { return false; }
+        if (parts.length != 3) {
+            return false;
+        }
         int iterations = Integer.parseInt(parts[0]);
         byte[] salt = Base64.getDecoder().decode(parts[1]);
         String expectedHash = encodePassword(rawPassword, salt, iterations);
@@ -30,23 +30,14 @@ public class SHA512PasswordEncoder {
     }
 
     private String encodePassword(String rawPassword, byte[] salt, int iterations) {
-        try {
-            MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+        String hashedPassword = encoder.hashWithSalt(rawPassword, salt);
 
-            md.update(salt);
-            byte[] encryptedPassword = md.digest(rawPassword.getBytes(StandardCharsets.UTF_8));
-
-            for (int i = 0; i < (1 << iterations); i++) {
-                md.update(encryptedPassword);
-                encryptedPassword = md.digest();
-            }
-
-            String base64Salt = Base64.getEncoder().encodeToString(salt);
-            String base64Hash = Base64.getEncoder().encodeToString(encryptedPassword);
-            return iterations + "$" + base64Salt + "$" + base64Hash;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("SHA-512 algorithm not found", e);
+        for (int i = 0; i < (1 << iterations); i++) {
+            hashedPassword = encoder.hash(hashedPassword);
         }
+
+        String base64Salt = Base64.getEncoder().encodeToString(salt);
+        return iterations + "$" + base64Salt + "$" + hashedPassword;
     }
 
     private byte[] generateSalt() {
