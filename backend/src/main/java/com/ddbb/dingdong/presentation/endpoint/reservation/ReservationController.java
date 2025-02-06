673d9d9db4c0538ef96ce0de42f1899d43955cae
keemjoonsung <kjs990114@naver.com>
2025-02-06 17:15:25 +0900

    [feat] 예매 토큰 발급 Controller
    기존 ReservationController에 token을 발급하는 엔드포인트 추가.
    url은 /api/users/reservations/requests
    
    #107

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/reservation/ReservationController.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/reservation/ReservationController.java
@@ -2,26 +2,26 @@ package com.ddbb.dingdong.presentation.endpoint.reservation;
 
 import com.ddbb.dingdong.application.exception.APIException;
 import com.ddbb.dingdong.application.usecase.reservation.GetReservationsUseCase;
+import com.ddbb.dingdong.application.usecase.reservation.RequestReservationUseCase;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.infrastructure.auth.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.annotation.LoginUser;
 import com.ddbb.dingdong.presentation.endpoint.reservation.exchanges.ReservationCategory;
+import com.ddbb.dingdong.presentation.endpoint.reservation.exchanges.ReservationRequestDTO;
 import com.ddbb.dingdong.presentation.endpoint.reservation.exchanges.SortType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users/reservations")
 public class ReservationController {
     private final GetReservationsUseCase getReservationsUseCase;
+    private final RequestReservationUseCase requestReservationUseCase;
 
     @GetMapping
     public ResponseEntity<GetReservationsUseCase.Result> getReservations(
@@ -42,5 +42,28 @@ public class ReservationController {
         }
 
         return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/requests")
+    public ResponseEntity<RequestReservationUseCase.Result> getReservations(
+            @LoginUser AuthUser user,
+            @RequestBody ReservationRequestDTO reservationRequestDTO
+    ) {
+        Long userId = user.id();
+        RequestReservationUseCase.Param param = new RequestReservationUseCase.Param(
+                userId,
+                reservationRequestDTO.getDirection(),
+                reservationRequestDTO.getReservationInfos().stream().map(
+                        reservationInfo -> new RequestReservationUseCase.Param.ReservationInfo(reservationInfo.getDate())
+                ).toList()
+        );
+        RequestReservationUseCase.Result result;
+        try {
+            result = requestReservationUseCase.execute(param);
+        } catch (DomainException ex) {
+            throw new APIException(ex, HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok().body(result);
     }
 }
