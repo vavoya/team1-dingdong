a38de278682c35fefc5803ac64b1f96ff800c12a
keemjoonsung <kjs990114@naver.com>
2025-02-06 11:09:00 +0900

    [feat] 예매 정보 확인용 jpa 조회 projection 인터페이스 생성
    #64

---
+++ backend/src/main/java/com/ddbb/dingdong/domain/reservation/repository/projection/UserReservationProjection.java
@@ -0,0 +1,18 @@
+package com.ddbb.dingdong.domain.reservation.repository.projection;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface UserReservationProjection {
+    Long getReservationId();
+    LocalDate getStartDate();
+    String getBusStopRoadNameAddress();
+    String getUserHomeStationName();
+    String getDirection();
+    LocalDateTime getExpectedArrivalTime();
+    String getReservationStatus();
+    String getBusStatus();
+    String getBusName();
+    LocalDateTime getBusStopArrivalTime();
+    Integer getTotalMinutes();
+}
