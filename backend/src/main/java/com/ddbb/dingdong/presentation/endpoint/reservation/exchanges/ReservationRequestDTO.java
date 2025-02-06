e347c1ab4f09d32f0f4f61377d15a6db55bf60d2
keemjoonsung <kjs990114@naver.com>
2025-02-06 17:17:59 +0900

    [feat] 예매 토큰 API request DTO 구현
    json 타입에 포함된 LocalDateTime을 Object로 변경하기위해 jsonFormat 어노테이션을 이용하여 DTO 구현
    
    #107

---
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/reservation/exchanges/ReservationRequestDTO.java
@@ -0,0 +1,28 @@
+package com.ddbb.dingdong.presentation.endpoint.reservation.exchanges;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class ReservationRequestDTO {
+    private String direction;
+    private List<ReservationInfo> reservationInfos;
+
+    @Getter
+    @NoArgsConstructor
+    public static class ReservationInfo {
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime date;
+    }
+}
