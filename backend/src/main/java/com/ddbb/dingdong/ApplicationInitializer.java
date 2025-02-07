d3965c9bdbe68d61ec07e676e39632ce75105acd
ji-woong-song <thdwldnd7@naver.com>
2025-02-07 14:21:00 +0900

    [feat]: 서버 실행시 테스트 유저 회원가입 보장 기능 추가
    softeer5th#117

---
+++ backend/src/main/java/com/ddbb/dingdong/ApplicationInitializer.java
@@ -0,0 +1,32 @@
+package com.ddbb.dingdong;
+
+import com.ddbb.dingdong.domain.user.entity.Home;
+import com.ddbb.dingdong.domain.user.entity.School;
+import com.ddbb.dingdong.domain.user.entity.User;
+import com.ddbb.dingdong.domain.user.repository.UserRepository;
+import com.ddbb.dingdong.infrastructure.auth.encrypt.SHA512PasswordEncoder;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ApplicationInitializer {
+    private final UserRepository userRepository;
+    private final SHA512PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        if (userRepository.findByEmail("test@test.com").isEmpty()) {
+            String password = passwordEncoder.encode("abcd1234!@");
+            Home home = new Home(null, 37.5143, 127.0294, 37.513716, 127.029790,"에티버스",null);
+            School school = new School(null, "seoul", "address", 1.0, 1.0);
+            User user = new User(null, "test", "test@test.com", password, LocalDateTime.now(), school, home);
+            userRepository.save(user);
+        }
+    }
+}
