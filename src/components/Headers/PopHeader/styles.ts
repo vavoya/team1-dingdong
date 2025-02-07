105ae2a73c44cdb741444e5d981263ec1f2746e6
admin <admin@admins-MacBook-Pro-4.local>
2025-02-07 15:32:37 +0900

    design: 폰트 재적용 되어 약간의 깜빡으로 제거

--- src/components/Headers/PopHeader/styles.ts
+++ src/components/Headers/PopHeader/styles.ts
@@ -1,33 +1,27 @@
 import styled from "styled-components";
-import {colors} from "@/styles/colors.ts";
-import {Body1SemiBold} from "@/styles/typography.ts";
-
+import { colors } from "@/styles/colors.ts";
+import { Body1SemiBold } from "@/styles/typography.ts";
 
 export const Wrapper = styled.div`
-    display: flex;
-    height: 60px;
-    padding: 0 20px;
-    justify-content: space-between;
-    align-items: center;
-    flex-shrink: 0;
-`
+  display: flex;
+  height: 60px;
+  padding: 0 20px;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+`;
 
 export const LeftBox = styled.div`
-    display: flex;
-    align-items: center;
-`
-
+  display: flex;
+  align-items: center;
+`;
 
 export const NavButton = styled.button`
-    width: 38px;
-    height: 38px;
-    flex-shrink: 0;
-`
+  width: 38px;
+  height: 38px;
+  flex-shrink: 0;
+`;
 
 export const Title = styled(Body1SemiBold)`
-    color: ${colors.gray100};
-
-    /* Body 1/Semibold */
-    font-family: Pretendard,serif;
-    font-style: normal;
-`
+  color: ${colors.gray100};
+`;
