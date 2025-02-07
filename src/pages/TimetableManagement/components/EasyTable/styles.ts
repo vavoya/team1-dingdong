4acf5691a488949a881cc79e16c39cf555faf953
vavoya <vavoya6324@gmail.com>
2025-02-07 14:02:07 +0900

    feat: HTML table 구조 생성 컴포넌트
    Table 태그가 사용되는 2곳이 존재한다(시간표 등록, 예매하기의 예매 목록)
    그 두 곳에서 Table을 쉽게, 그리고 공통 되게 사용할 수 있게 기존 `CreateTable`을 삭제하고, 범용적인 `EasyTable` 컴포넌트를 제작해서 모듈화 후 사용

--- src/pages/TimetableManagement/components/EasyTable/styles.ts
+++ src/pages/TimetableManagement/components/EasyTable/styles.ts
@@ -7,14 +7,16 @@ export const Table = styled.table.withConfig({
     table-layout: fixed;
     border-collapse: collapse;
 
-    thead, tbody {
-        tr *:not(:first-child) {
+    /* thead와 tbody의 직계 tr의 직계 자식 중 첫 번째가 아닌 요소에 패딩 적용 */
+    > thead > tr, > tbody > tr {
+        > *:not(:first-child) {
             padding-left: ${({ column = "0px" }) => column};
         }
     }
 
-    tbody {
-        td, th {
+    /* tbody 내의 직계 tr의 직계 td, th에 패딩 적용 */
+    > tbody > tr {
+        > td, > th {
             padding-top: ${({ row = "0px" }) => row};
         }
     }
