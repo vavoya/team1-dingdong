85959591039e8e685cdced7680f694f8e2e4e5b4
keemjoonsung <kjs990114@naver.com>
2025-02-07 12:44:13 +0900

    [fix] Reservation Entity Setter 추가
    모든 필드에대해 도메인 메소드를 만들기엔 비효율적이라고 생각하여, Entity 무결성을 조금 위배하더라도 Setter를 추가
    
    #19

--- backend/src/main/java/com/ddbb/dingdong/domain/reservation/entity/Reservation.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/reservation/entity/Reservation.java
@@ -3,22 +3,22 @@ package com.ddbb.dingdong.domain.reservation.entity;
 import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.domain.reservation.entity.vo.ReservationStatus;
 import com.ddbb.dingdong.domain.reservation.entity.vo.ReservationType;
+import com.ddbb.dingdong.domain.reservation.service.ReservationErrors;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id = null;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -42,6 +42,26 @@ public class Reservation {
     @Column(nullable = false)
     private Long userId;
 
-    @OneToOne(mappedBy = "reservation")
+    @OneToOne(mappedBy = "reservation", cascade = CascadeType.ALL)
     private Ticket ticket;
+
+    public void cancel() {
+        if(!ReservationStatus.PENDING.equals(this.status)) {
+            throw ReservationErrors.CANCELLATION_NOT_ALLOWED.toException();
+        }
+
+        this.status = ReservationStatus.CANCELED;
+    }
+
+    public void allocateBus(Ticket ticket) {
+        if(ticket == null || ticket.getBusScheduleId() == null || ticket.getBusStopId() == null) {
+            throw ReservationErrors.INVALID_BUS_TICKET.toException();
+        }
+        if(!ReservationStatus.PENDING.equals(this.status)) {
+            throw ReservationErrors.ALLOCATION_NOT_ALLOWED.toException();
+        }
+
+        this.ticket = ticket;
+        this.status = ReservationStatus.ALLOCATED;
+    }
 }
