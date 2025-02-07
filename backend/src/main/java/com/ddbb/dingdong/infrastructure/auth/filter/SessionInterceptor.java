7c5ed31b85655f53bf60ccdc308bdeafe84bce0c
ji-woong-song <thdwldnd7@naver.com>
2025-02-07 14:07:32 +0900

    [fix]: Authentication 검사 예외 추가
    - Preflight 요청 시 세션 정보가 없어도 Preflight 응답을 전송할 수 있도록 요청 Method가 OPTIONS 일 때 Authentication 검사를 하지 않도록 함.
    
    softeer5th#117

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/filter/SessionInterceptor.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/auth/filter/SessionInterceptor.java
@@ -14,6 +14,9 @@ public class SessionInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (request.getMethod().equals("OPTIONS")) {
+            return true;
+        }
         if(authenticationManager.getAuthentication() == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
