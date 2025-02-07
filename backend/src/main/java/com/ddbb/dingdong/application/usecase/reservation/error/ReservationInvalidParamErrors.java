fb5aabe0cb101e93490ab491f52d4021bd6dbbcf
keemjoonsung <kjs990114@naver.com>
2025-02-07 12:43:21 +0900

    [fix] 예매 형식 에러 enum 제거
    json 포맷이 아니면 Object Mapper에서 예외를 던지는데, 그것을 처리해줄 enum 필드 만들었었는데 불필요하다고 생각하였고, 이를 제거
    
    #19

--- backend/src/main/java/com/ddbb/dingdong/application/usecase/reservation/error/ReservationInvalidParamErrors.java
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/reservation/error/ReservationInvalidParamErrors.java
@@ -4,9 +4,7 @@ import com.ddbb.dingdong.application.exception.InvalidParamErrorInfo;
 
 public enum ReservationInvalidParamErrors implements InvalidParamErrorInfo {
     INVALID_DIRECTION("운행 방향이 올바르게 지정 되지 않았습니다.", "direction"),
-    INVALID_DATE("해당 예약 시간은 지원하지 않습니다.","date"),
-
-    INVALID_RESERVATION_FORMAT("올바른 요청 형식이 아닙니다.","reservationInfos"),
+    INVALID_DATE("예약 시간의 형식이 올바르지 않습니다.","date"),
     ;
 
     private final String desc;
