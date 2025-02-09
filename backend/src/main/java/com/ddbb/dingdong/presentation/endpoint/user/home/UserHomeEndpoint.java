9172e87b79ad2fc14b52a8bfb35bc77d5f921af5
성필 <philip000412@gmail.com>
2025-02-09 17:01:13 +0900

    [feat]: 집 위치 수정 API Use Case 생성

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/user/home/UserHomeEndpoint.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/user/home/UserHomeEndpoint.java
@@ -2,21 +2,21 @@ package com.ddbb.dingdong.presentation.endpoint.user.home;
 
 import com.ddbb.dingdong.application.exception.APIException;
 import com.ddbb.dingdong.application.usecase.user.GetHomeLocationUseCase;
+import com.ddbb.dingdong.application.usecase.user.PutHomeLocationUseCase;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.infrastructure.auth.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.annotation.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users/home")
 public class UserHomeEndpoint {
     private final GetHomeLocationUseCase getHomeLocationUseCase;
+    private final PutHomeLocationUseCase putHomeLocationUseCase;
 
     @GetMapping("/locations")
     public ResponseEntity<GetHomeLocationUseCase.Result> getUserHomeLocations(
@@ -31,6 +31,22 @@ public class UserHomeEndpoint {
             throw new APIException(ex, HttpStatus.NOT_FOUND);
         }
 
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PutMapping
+    public ResponseEntity<PutHomeLocationUseCase.Result> updateUserHomeLocation(
+            @LoginUser AuthUser user,
+            @RequestBody UpdateHomeLocationDto dto
+    ) {
+        Long userId = user.id();
+        PutHomeLocationUseCase.Param param = new PutHomeLocationUseCase.Param(userId, dto);
+        PutHomeLocationUseCase.Result result;
+        try {
+            result = putHomeLocationUseCase.execute(param);
+        } catch (DomainException ex) {
+            throw new APIException(ex, HttpStatus.NOT_FOUND);
+        }
         return ResponseEntity.ok().body(result);
     }
 }
