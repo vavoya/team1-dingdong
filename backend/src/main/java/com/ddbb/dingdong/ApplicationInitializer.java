057d8efa5dde4298ed9d9d660fbfcf8480051e80
성필 <philip000412@gmail.com>
2025-02-09 17:01:47 +0900

    [fix]: 비밀번호 암호화 클래스 변경

--- backend/src/main/java/com/ddbb/dingdong/ApplicationInitializer.java
+++ backend/src/main/java/com/ddbb/dingdong/ApplicationInitializer.java
@@ -4,7 +4,7 @@ import com.ddbb.dingdong.domain.user.entity.Home;
 import com.ddbb.dingdong.domain.user.entity.School;
 import com.ddbb.dingdong.domain.user.entity.User;
 import com.ddbb.dingdong.domain.user.repository.UserRepository;
-import com.ddbb.dingdong.infrastructure.auth.encrypt.SHA512PasswordEncoder;
+import com.ddbb.dingdong.infrastructure.auth.encrypt.PasswordEncoder;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 @Slf4j
 public class ApplicationInitializer {
     private final UserRepository userRepository;
-    private final SHA512PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @PostConstruct
     public void init() {
@@ -25,7 +25,8 @@ public class ApplicationInitializer {
             String password = passwordEncoder.encode("abcd1234!@");
             Home home = new Home(null, 37.5143, 127.0294, 37.513716, 127.029790,"에티버스",null);
             School school = new School(null, "seoul", "address", 1.0, 1.0);
-            User user = new User(null, "test", "test@test.com", password, LocalDateTime.now(), school, home);
+            User user = new User(null, "test", "test@test.com", password, LocalDateTime.now(), school, null);
+            user.associateHome(home);
             userRepository.save(user);
         }
     }
