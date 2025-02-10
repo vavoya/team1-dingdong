835ed0456755022bd26b6e3df31a1a9ebbd4007a
ji-woong-song <thdwldnd7@naver.com>
2025-02-10 15:08:33 +0900

    [feat]: 취소가 되지 않은 예약 건수를 조회하는 기능 추가 - BusScheduleQueryRepository
    softeer5th#133

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/BusScheduleQueryRepository.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/BusScheduleQueryRepository.java
@@ -1,6 +1,7 @@
 package com.ddbb.dingdong.domain.transportation.repository;
 
 import com.ddbb.dingdong.domain.transportation.entity.BusSchedule;
+import com.ddbb.dingdong.domain.transportation.repository.projection.BusReservedSeatProjection;
 import com.ddbb.dingdong.domain.transportation.repository.projection.ScheduleTimeProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,13 @@ public interface BusScheduleQueryRepository extends JpaRepository<BusSchedule, L
 
     @Query("SELECT DISTINCT bs.departureTime as time FROM BusSchedule bs WHERE bs.schoolId = :schoolId and bs.status = 'READY'")
     List<ScheduleTimeProjection> findAvailableGoHomeBusTime(@Param("schoolId") Long schoolId);
+
+    @Query("SELECT COUNT(t.id) as reservedSeatCount , bs.id as busScheduleId " +
+            "FROM Ticket t " +
+            "JOIN BusSchedule bs ON t.busScheduleId = bs.id " +
+            "JOIN Reservation r ON t.id = r.ticket.id " +
+            "WHERE bs.id in :busScheduleIds " +
+            "AND r.status != 'CANCELED' " +
+            "GROUP BY bs.id")
+    List<BusReservedSeatProjection> findReservedSeatCount(@Param("busScheduleIds") List<Long> busScheduleIds);
 }
