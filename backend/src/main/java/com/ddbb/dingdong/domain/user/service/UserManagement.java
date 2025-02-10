e77a2dc8dee5285e29305b79e643164aa0326e65
popopy0412 <philip000412@gmail.com>
2025-02-10 17:19:28 +0900

    [refactor]: 사용자 인증 로직을 User -> Auth Domain으로 변경

--- backend/src/main/java/com/ddbb/dingdong/domain/user/service/UserManagement.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/user/service/UserManagement.java
@@ -1,10 +1,6 @@
 package com.ddbb.dingdong.domain.user.service;
 
-import com.ddbb.dingdong.domain.user.entity.User;
 import com.ddbb.dingdong.domain.user.repository.UserRepository;
-import com.ddbb.dingdong.infrastructure.auth.AuthUser;
-import com.ddbb.dingdong.infrastructure.auth.AuthenticationManager;
-import com.ddbb.dingdong.infrastructure.auth.encrypt.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +8,4 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserManagement {
     private final UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
-
-    public void login(String email, String password) {
-        User user = userRepository.findByEmail(email).orElseThrow(UserErrors.NOT_FOUND::toException);
-        if(!passwordEncoder.matches(password, user.getPassword())) throw UserErrors.NOT_MATCHED_PASSWORD.toException();
-        authenticationManager.setAuthentication(new AuthUser(user.getId()));
-    }
 }
