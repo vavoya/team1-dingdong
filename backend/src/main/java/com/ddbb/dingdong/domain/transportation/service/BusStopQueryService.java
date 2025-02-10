c7cc5c46ec30044a14bb5d6f124fe2a6b6517b24
ji-woong-song <thdwldnd7@naver.com>
2025-02-10 16:00:20 +0900

    [refactor]: BusStopQueryService stream 문과  AvailableBusStopDistance 레코드 도입으로 코드 구조 변경
    softeer5th#133

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/service/BusStopQueryService.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/service/BusStopQueryService.java
@@ -1,20 +1,31 @@
 package com.ddbb.dingdong.domain.transportation.service;
 
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
+
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
 import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.domain.transportation.repository.BusStopQueryRepository;
 import com.ddbb.dingdong.domain.transportation.repository.projection.AvailableBusStopProjection;
 import com.ddbb.dingdong.util.GeoUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class BusStopQueryService {
     private static final int THRESHOLD_METER = 1000;
+
+    public record AvailableBusStopDistance(AvailableBusStopProjection busStop, double distance)
+            implements Comparable<AvailableBusStopDistance> {
+        @Override
+        public int compareTo(AvailableBusStopDistance o) {
+            return Double.compare(this.distance, o.distance);
+        }
+    }
 
     private final BusStopQueryRepository busStopQueryRepository;
 
@@ -25,31 +36,14 @@ public class BusStopQueryService {
             case TO_HOME -> busStopQueryRepository.findAvailableGoHomeBusStop(time, schoolId);
             case TO_SCHOOL -> busStopQueryRepository.findAvailableGoSchoolBusStop(time, schoolId);
         };
-        Map<Long, List<AvailableBusStopProjection>> grouped = projections.stream()
-                .collect(Collectors.groupingBy(AvailableBusStopProjection::getPathId, Collectors.toList()
-        ));
-
-        List<AvailableBusStopProjection> result = new ArrayList<>();
-        for (List<AvailableBusStopProjection> group : grouped.values()) {
-            if (group.isEmpty()) continue;
-
-            AvailableBusStopProjection minItem = group.get(0);
-            double minDistance = GeoUtil.haversine(latitude, longitude, minItem.getLatitude(), minItem.getLongitude());
-            int itemCount = group.size();
-            for (int i = 1; i < itemCount; i++) {
-                AvailableBusStopProjection item = group.get(i);
-
-                double currentDistance = GeoUtil.haversine(latitude, longitude, item.getLatitude(), item.getLongitude());
-                if (currentDistance < minDistance) {
-                    minDistance = currentDistance;
-                    minItem = item;
-                }
-            }
-            if (minDistance > THRESHOLD_METER) {
-                continue;
-            }
-            result.add(minItem);
-        }
-        return result;
+        return projections.stream()
+            .map(projection -> {
+                double distance = GeoUtil.haversine(latitude, longitude, projection.getLatitude(), projection.getLongitude());
+                return new AvailableBusStopDistance(projection, distance);
+            })
+            .filter(item -> item.distance() <= THRESHOLD_METER)
+            .sorted(Comparator.comparingDouble(AvailableBusStopDistance::distance))
+            .map(AvailableBusStopDistance::busStop)
+            .toList();
     }
 }
