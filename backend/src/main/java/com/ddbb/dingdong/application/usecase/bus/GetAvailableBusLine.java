de2795ae16bdf10cbed7bc26e696352bc8f134dd
ji-woong-song <thdwldnd7@naver.com>
2025-02-10 16:02:16 +0900

    [feat]: GetAvailableBusLine 유스케이스 구현
    softeer5th#133

--- backend/src/main/java/com/ddbb/dingdong/application/usecase/bus/GetAvailableBusLine.java
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/bus/GetAvailableBusLine.java
@@ -1,23 +1,68 @@
 package com.ddbb.dingdong.application.usecase.bus;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
 import com.ddbb.dingdong.application.common.Params;
 import com.ddbb.dingdong.application.common.UseCase;
 import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.domain.transportation.repository.BusScheduleQueryRepository;
+import com.ddbb.dingdong.domain.transportation.repository.projection.AvailableBusStopProjection;
+import com.ddbb.dingdong.domain.transportation.repository.projection.BusReservedSeatProjection;
+import com.ddbb.dingdong.domain.transportation.service.BusStopQueryService;
+import com.ddbb.dingdong.domain.user.repository.UserQueryRepository;
+import com.ddbb.dingdong.domain.user.repository.projection.HomeStationProjection;
+import com.ddbb.dingdong.domain.user.service.UserErrors;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
+@Service
 @RequiredArgsConstructor
 public class GetAvailableBusLine implements UseCase<GetAvailableBusLine.Param, GetAvailableBusLine.Response> {
     private final BusScheduleQueryRepository busScheduleQueryRepository;
+    private final UserQueryRepository userQueryRepository;
+    private final BusStopQueryService busStopQueryService;
 
     @Override
     public Response execute(Param param) {
-        return null;
+        HomeStationProjection homeStation = userQueryRepository.findHomeStationLocationWithSchoolId(param.userId)
+                .orElseThrow(UserErrors.NOT_FOUND::toException);
+        List<AvailableBusStopProjection> busStops = busStopQueryService.findAvailableBusStops(
+                homeStation.getSchoolId(),
+                param.direction,
+                param.time,
+                homeStation.getStationLongitude(),
+                homeStation.getStationLatitude()
+        );
+        List<Long> busIds = busStops.stream().map(AvailableBusStopProjection::getBusScheduleId).toList();
+        Map<Long, Long> reservedCount = busScheduleQueryRepository.findReservedSeatCount(busIds)
+                .stream()
+                .collect(Collectors.toMap(BusReservedSeatProjection::getBusScheduleId, BusReservedSeatProjection::getReservedSeatCount));
+
+        List<Response.Item> items = busStops.stream().map(busStop -> {
+            int count = Math.toIntExact(reservedCount.get(busStop.getBusScheduleId()));
+            Response.BusStopInfo busStopInfo = Response.BusStopInfo.builder()
+                    .name(busStop.getBusStopName())
+                    .time(busStop.getBusStopTime())
+                    .latitude(busStop.getLatitude())
+                    .longitude(busStop.getLongitude())
+                    .build();
+            Response.BusInfo busInfo = Response.BusInfo.builder()
+                    .name(busStop.getBusName())
+                    .reservedSeat(count)
+                    .totalSeat(15)
+                    .build();
+            return new Response.Item(busStop.getPathId(), busStopInfo, busInfo);
+        })
+        .toList();
+        return new Response(items);
     }
 
     @Getter
@@ -36,11 +81,13 @@ public class GetAvailableBusLine implements UseCase<GetAvailableBusLine.Param, G
         @Getter
         @AllArgsConstructor
         public static class Item {
+            private Long pathId;
             private BusStopInfo busStop;
             private BusInfo busInfo;
         }
 
         @Getter
+        @Builder
         @AllArgsConstructor
         public static class BusStopInfo {
             private String name;
@@ -50,6 +97,7 @@ public class GetAvailableBusLine implements UseCase<GetAvailableBusLine.Param, G
         }
 
         @Getter
+        @Builder
         @AllArgsConstructor
         public static class BusInfo {
             private String name;
