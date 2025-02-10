fe7b476d8b6d646f13c766cef14a3a0158563e06
ji-woong-song <thdwldnd7@naver.com>
2025-02-10 16:34:12 +0900

    [feat]: 가능한 버스 정류장 정보 조회 엔드포인트 추가
    softeer5th#133

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/bus/BusController.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/bus/BusController.java
@@ -1,12 +1,14 @@
 package com.ddbb.dingdong.presentation.endpoint.bus;
 
 import com.ddbb.dingdong.application.exception.APIException;
+import com.ddbb.dingdong.application.usecase.bus.GetAvailableBusLine;
 import com.ddbb.dingdong.application.usecase.bus.GetBusSchedulesUseCase;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.infrastructure.auth.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.annotation.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+
 @RestController
 @RequestMapping("/api/bus")
 @RequiredArgsConstructor
 public class BusController {
     private final GetBusSchedulesUseCase getBusSchedulesUseCase;
+    private final GetAvailableBusLine getAvailableBusLine;
 
     @GetMapping("/schedule/time")
     public ResponseEntity<GetBusSchedulesUseCase.Response> getBusSchedules(
@@ -28,6 +33,21 @@ public class BusController {
         try {
             GetBusSchedulesUseCase.Param param = new GetBusSchedulesUseCase.Param(authUser.id(), direction);
             return ResponseEntity.ok(getBusSchedulesUseCase.execute(param));
+        } catch (DomainException e) {
+            throw new APIException(e, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/available")
+    public ResponseEntity<GetAvailableBusLine.Response> getBusSchedules(
+            @LoginUser AuthUser authUser,
+            @RequestParam("direction") Direction direction,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam("time") ZonedDateTime time
+    ) {
+        try {
+            GetAvailableBusLine.Param param = new GetAvailableBusLine.Param(authUser.id(), time.toLocalDateTime(), direction);
+            GetAvailableBusLine.Response result = getAvailableBusLine.execute(param);
+            return ResponseEntity.ok(result);
         } catch (DomainException e) {
             throw new APIException(e, HttpStatus.NOT_FOUND);
         }
