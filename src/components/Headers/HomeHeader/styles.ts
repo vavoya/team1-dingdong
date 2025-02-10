48aad53e1caf0cb51c9c77fdc08919544806c54c
vavoya <vavoya6324@gmail.com>
2025-02-10 17:42:31 +0900

    style: 헤더 컴포넌트 3개 z-index 추가

--- src/components/Headers/HomeHeader/styles.ts
+++ src/components/Headers/HomeHeader/styles.ts
@@ -4,13 +4,13 @@ import {colors} from "@/styles/colors.ts";
 export const Wrapper = styled.header`
     position: sticky;
     top: 0;
-    background: ${colors.gray20};
-    
+    z-index: 1;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
     height: 60px;
     padding: 0 20px;
+    justify-content: space-between;
+    align-items: center;
+    background: ${colors.gray20};
 `
 
 export const NavList = styled.ul`
--- src/components/Headers/PopHeader/styles.ts
+++ src/components/Headers/PopHeader/styles.ts
@@ -3,16 +3,18 @@ import { colors } from "@/styles/colors.ts";
 import { Body1SemiBold } from "@/styles/typography.ts";
 
 export const Wrapper = styled.div`
-  position: sticky;
-  top: 0;
-
-  display: flex;
-  height: 60px;
-  padding: 0 20px;
-  justify-content: space-between;
-  align-items: center;
-  flex-shrink: 0;
-`;
+    position: sticky;
+    top: 0;
+    z-index: 1;
+    display: flex;
+    height: 60px;
+    padding: 0 20px;
+    justify-content: space-between;
+    align-items: center;
+    flex-shrink: 0;
+    border-bottom: 1px solid ${colors.gray20};
+    background: ${colors.gray0};
+`
 
 export const LeftBox = styled.div`
   display: flex;
