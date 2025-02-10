894587078291e2a80b7cb98e069e924694083ec9
popopy0412 <philip000412@gmail.com>
2025-02-10 17:49:44 +0900

    [feat]: 이메일 검증 DTO 추가

--- backend/src/main/java/com/ddbb/dingdong/application/usecase/auth/CheckEmailUseCase.java
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/auth/CheckEmailUseCase.java
@@ -5,6 +5,7 @@ import com.ddbb.dingdong.application.common.UseCase;
 import com.ddbb.dingdong.application.usecase.auth.errors.AuthParamErrors;
 import com.ddbb.dingdong.application.usecase.auth.util.EmailValidator;
 import com.ddbb.dingdong.domain.auth.service.AuthManagement;
+import com.ddbb.dingdong.presentation.endpoint.auth.CheckEmailDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -18,18 +19,20 @@ public class CheckEmailUseCase implements UseCase<CheckEmailUseCase.Param, Void>
     @Override
     public Void execute(Param param) {
         param.validate();
-        authManagement.checkEmail(param.email);
+        String email = param.dto.getEmail();
+        authManagement.checkEmail(email);
         return null;
     }
 
     @Getter
     @AllArgsConstructor
     public static class Param implements Params {
-        private String email;
+        private CheckEmailDto dto;
 
         @Override
         public boolean validate() {
-            if (email.isBlank()) {
+            String email = dto.getEmail();
+            if (email == null || email.isBlank()) {
                 throw AuthParamErrors.EMAIL_REQUIRED.toException();
             } else if (!EmailValidator.isValidEmail(email)) {
                 throw AuthParamErrors.INVALID_EMAIL_FORMAT.toException();
---
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/auth/CheckEmailDto.java
@@ -0,0 +1,8 @@
+package com.ddbb.dingdong.presentation.endpoint.auth;
+
+import lombok.Data;
+
+@Data
+public class CheckEmailDto {
+    String email;
+}
