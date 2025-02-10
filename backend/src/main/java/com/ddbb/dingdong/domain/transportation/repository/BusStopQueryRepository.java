9de34fb35f05159491a010627bfb99347ec5a94e
ji-woong-song <thdwldnd7@naver.com>
2025-02-10 13:44:41 +0900

    [feat]: 이용가능한 노선의 모든 정류장 정보를 불러오는 메서드 추가  - BusStopQueryRepository
     BusSchedule 이 'READY'이고, 각 목표시간이 인자로 넘겨준 시간이고, 목표 학교 아이디가 넘겨준 학교 아이디와 일치하는 경로의 정류장 정보
    
    softeer5th#133

---
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/BusStopQueryRepository.java
@@ -0,0 +1,44 @@
+package com.ddbb.dingdong.domain.transportation.repository;
+
+import com.ddbb.dingdong.domain.transportation.entity.BusStop;
+import com.ddbb.dingdong.domain.transportation.repository.projection.AvailableBusStopProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface BusStopQueryRepository extends JpaRepository<BusStop, Long> {
+    @Query("SELECT bst.roadNameAddress as busStopName, bst.latitude as latitude, bst.longitude as longitude, " +
+            "bst.expectedArrivalTime as busStopTime, b.name as busName, p.id as pathId, bs.id as busScheduleId " +
+            "FROM BusStop bst " +
+            "JOIN Ticket t ON t.busStopId = bst.id " +
+            "JOIN BusSchedule bs ON t.busScheduleId = bs.id " +
+            "JOIN Path p ON p.busSchedule.id = bs.id  " +
+            "JOIN Bus b ON b.id = bs.bus.id " +
+            "WHERE bs.departureTime = :departureTime AND bs.schoolId = :schoolId " +
+            "AND bs.status = 'READY'")
+    List<AvailableBusStopProjection> findAvailableGoHomeBusStop(
+            @Param("departureTime") LocalDateTime departureTime,
+            @Param("schoolId") Long schoolId
+    );
+
+    @Query("SELECT bst.roadNameAddress as busStopName, bst.latitude as latitude, bst.longitude as longitude, " +
+            "bst.expectedArrivalTime as busStopTime, b.name as busName, p.id as pathId, bs.id as busScheduleId " +
+            "FROM BusStop bst " +
+            "JOIN Ticket t ON t.busStopId = bst.id " +
+            "JOIN BusSchedule bs ON t.busScheduleId = bs.id " +
+            "JOIN Path p ON p.busSchedule.id = bs.id  " +
+            "JOIN Bus b ON b.id = bs.bus.id " +
+            "WHERE bs.arrivalTime = :arrivalTime AND bs.schoolId = :schoolId " +
+            "AND bs.status = 'READY'")
+    List<AvailableBusStopProjection>  findAvailableGoSchoolBusStop(
+            @Param("arrivalTime") LocalDateTime arrivalTime,
+            @Param("schoolId") Long schoolId
+    );
+
+
+}
---
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/projection/AvailableBusStopProjection.java
@@ -0,0 +1,13 @@
+package com.ddbb.dingdong.domain.transportation.repository.projection;
+
+import java.time.LocalDateTime;
+
+public interface AvailableBusStopProjection {
+    Long getPathId();
+    LocalDateTime getBusStopTime();
+    String getBusStopName();
+    Double getLongitude();
+    Double getLatitude();
+    String getBusName();
+    Long getBusScheduleId();
+}
