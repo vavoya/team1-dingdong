2c47d65941b04277deb87c58285b573b72020456
popopy0412 <philip000412@gmail.com>
2025-02-10 19:58:42 +0900

    [refactor]: User 클래스 Builder 추가

--- backend/src/main/java/com/ddbb/dingdong/domain/user/entity/User.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/user/entity/User.java
@@ -2,6 +2,7 @@ package com.ddbb.dingdong.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
