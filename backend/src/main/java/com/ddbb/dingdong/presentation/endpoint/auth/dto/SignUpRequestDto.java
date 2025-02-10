1ab722511232b8a30d7905c56333b02710c715ec
popopy0412 <philip000412@gmail.com>
2025-02-10 19:54:49 +0900

    [feat]: 회원가입 요청 DTO 생성

---
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/auth/dto/SignUpRequestDto.java
@@ -0,0 +1,24 @@
+package com.ddbb.dingdong.presentation.endpoint.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class SignUpRequestDto {
+    private String name;
+    private String email;
+    private String password;
+
+    @Data
+    @AllArgsConstructor
+    @RequiredArgsConstructor
+    public static class Home {
+        private final Double houseLatitude;
+        private final Double houseLongitude;
+        private Double stationLatitude;
+        private Double stationLongitude;
+        private String stationName;
+    }
+}
