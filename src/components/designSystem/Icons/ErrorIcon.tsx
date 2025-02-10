2fe0ff405b391db7c454982d17ff55d5cb886396
vavoya <vavoya6324@gmail.com>
2025-02-10 17:25:30 +0900

    feat: 에러 아이콘 추가
    - 에러 모달에  사용된다

---
+++ src/components/designSystem/Icons/ErrorIcon.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function ErrorIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+            <path d="M9.67163 16.1716C11.2337 14.6095 13.7664 14.6095 15.3285 16.1716M9.5 10H9.51M15.5 10H15.51M21.5 12C21.5 16.9706 17.4706 21 12.5 21C7.52944 21 3.5 16.9706 3.5 12C3.5 7.02944 7.52944 3 12.5 3C17.4706 3 21.5 7.02944 21.5 12Z" stroke="#222235" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
