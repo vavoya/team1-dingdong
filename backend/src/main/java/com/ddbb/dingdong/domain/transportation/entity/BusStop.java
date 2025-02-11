a9a6877c58f50527e78d94994e5c27fe1cadc6f0
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:17:27 +0900

    [fix] BusStop Setter 추가
    #149

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/entity/BusStop.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/entity/BusStop.java
@@ -4,12 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BusStop {
