90ba5d17d3a9371ad823b43051e98ae61790a306
ji-woong-song <thdwldnd7@naver.com>
2025-02-11 14:43:53 +0900

    [feat]: SocketSubscriber 추가
    - 발행자에게서 받아온 좌표를 WebSocket을 통해 전달하는 구독자 객체 추가
    
    softeer5th#106

---
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/bus/simulator/subscription/subscriber/SocketSubscriber.java
@@ -0,0 +1,61 @@
+package com.ddbb.dingdong.infrastructure.bus.simulator.subscription.subscriber;
+
+import com.ddbb.dingdong.infrastructure.bus.simulator.subscription.BusSubscriptionManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.geo.Point;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+import java.util.concurrent.Flow;
+
+@Slf4j
+public class SocketSubscriber extends CancelableSubscriber<Point> {
+    private final long busId;
+    private final long userId;
+    private final WeakReference<WebSocketSession> weakRef;
+    private final BusSubscriptionManager busSubscriptionManager;
+
+    public SocketSubscriber(long busId, long userId, WebSocketSession session, BusSubscriptionManager busSubscriptionManager) {
+        Objects.requireNonNull(session);
+        this.busId = busId;
+        this.userId = userId;
+        this.weakRef = new WeakReference<>(session);
+        this.busSubscriptionManager = busSubscriptionManager;
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1L);
+    }
+
+    @Override
+    public void onNext(Point item) {
+        WebSocketSession webSocketSession = weakRef.get();
+        if (webSocketSession == null) {
+            this.busSubscriptionManager.unsubscribe(busId, userId);
+            return ;
+        }
+        String message = String.format("%.15f %.15f", item.getY(), item.getY());
+        try {
+            webSocketSession.sendMessage(new TextMessage(message));
+        } catch (IOException e) {
+            this.busSubscriptionManager.unsubscribe(busId, userId);
+            return;
+        }
+        this.subscription.request(1L);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error(throwable.getMessage(), throwable);
+        this.busSubscriptionManager.unsubscribe(busId, userId);
+    }
+
+    @Override
+    public void onComplete() {
+    }
+}
