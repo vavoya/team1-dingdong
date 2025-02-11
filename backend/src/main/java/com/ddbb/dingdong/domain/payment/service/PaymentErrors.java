32c24c4e073577b026d6867cc778144bb2a5650c
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:27:57 +0900

    [feat] 결제 환불 에러 추가
    #149

--- backend/src/main/java/com/ddbb/dingdong/domain/payment/service/PaymentErrors.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/payment/service/PaymentErrors.java
@@ -6,6 +6,7 @@ public enum PaymentErrors implements ErrorInfo {
     WALLET_NOT_FOUND("해당 유저의 잔고를 확인 할 수 없습니다."),
 
     INSUFFICIENT_BALANCE("잔고가 부족합니다."),
+    ALREADY_REFUNDED("해당 예매는 이미 환불이 처리되었습니다."),
     ;
 
     private final String desc;
