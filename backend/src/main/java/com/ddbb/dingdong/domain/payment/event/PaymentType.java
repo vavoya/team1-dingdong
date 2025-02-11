cb2ddd84cf2f1ef48169d8f84506475b30826ebc
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:31:19 +0900

    [remove] 불필요 파일 삭제
    #149

--- backend/src/main/java/com/ddbb/dingdong/domain/payment/event/PaymentType.java
+++
@@ -1,6 +0,0 @@
-package com.ddbb.dingdong.domain.payment.event;
-
-public enum PaymentType {
-    PAY,
-    REFUND,
-}
--- backend/src/main/java/com/ddbb/dingdong/domain/reservation/repository/projection/UserReservationLocationProjection.java
+++
@@ -1,7 +0,0 @@
-package com.ddbb.dingdong.domain.reservation.repository.projection;
-
-public interface UserReservationLocationProjection {
-    Long getReservationId();
-    Double getUserLat();
-    Double getUserLon();
-}
