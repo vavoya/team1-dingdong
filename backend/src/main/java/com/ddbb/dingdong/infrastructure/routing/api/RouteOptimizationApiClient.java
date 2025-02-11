fec5f1d197d07230fbfc17f8fa773440b1228fab
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:31:05 +0900

    [remove] 불필요 인터페이스 삭제
    #149

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/routing/api/RouteOptimizationApiClient.java
+++
@@ -1,8 +0,0 @@
-package com.ddbb.dingdong.infrastructure.routing.api;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface RouteOptimizationApiClient<T, R> {
-    R getRouteOptimization(T request);
-}
