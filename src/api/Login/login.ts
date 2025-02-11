d14a63169d22c0b5a5d676cbe5e2ef1ad99b8204
admin <admin@admins-MacBook-Pro-4.local>
2025-02-12 00:04:20 +0900

    feat: 로그인 API 요청 hook, api 코드 추가

---
+++ src/api/Login/login.ts
@@ -0,0 +1,13 @@
+import { axiosInstance } from "@/api";
+import { LoginDataType } from "@/hooks/Login/useLogin";
+
+export async function postLogin(loginFormData: LoginDataType) {
+  try {
+    return axiosInstance.post("/api/login", {
+      loginFormData,
+    });
+  } catch (err: any) {
+    console.error("로그인 실패:", err);
+    throw new Error(err);
+  }
+}
