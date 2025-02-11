352efa900f6448f7714c5e2e88ab9186ec10f074
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:18:46 +0900

    [feat] cluster label prefix 사용하도록 변경
    클러스터 할때마다, cluster label이 0으로 다시 초기화되므로 유니크한 cluster label이 필요하다고 판단해서,
    direction과 dingdongtime을 조합해 클러스터 라벨 prefix 생성
    
    #149

--- backend/src/main/java/com/ddbb/dingdong/domain/clustering/service/ClusteringService.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/clustering/service/ClusteringService.java
@@ -3,10 +3,12 @@ package com.ddbb.dingdong.domain.clustering.service;
 import com.ddbb.dingdong.domain.clustering.entity.Location;
 import com.ddbb.dingdong.domain.clustering.repository.LocationRepository;
 import com.ddbb.dingdong.domain.clustering.service.error.ClusterErrors;
+import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.infrastructure.clustering.ElkiCluster;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,12 +17,13 @@ public class ClusteringService {
     private final ElkiCluster elkiCluster;
     private final LocationRepository locationRepository;
 
-    public void cluster(List<Location> locationInfos) {
-        List<Location> clusters = elkiCluster.elkiDBScan(locationInfos,3, 2);
+    public void cluster(List<Location> allLocations, Direction direction, LocalDateTime dingdongTime) {
+        String labelPrefix = direction.name() + "_" + dingdongTime + "_";
+        List<Location> clusters = elkiCluster.elkiDBScan(allLocations,3, 2, labelPrefix);
         locationRepository.saveAll(clusters);
     }
 
-    public List<Location> findByClusterLabel(Long clusterLabel) {
+    public List<Location> findByClusterLabel(String clusterLabel) {
         List<Location> cluster = locationRepository.findAllByClusterLabel(clusterLabel);
         if(cluster.isEmpty()) {
             throw ClusterErrors.NOT_FOUND.toException();
@@ -28,4 +31,9 @@ public class ClusteringService {
 
         return cluster;
     }
+
+    public void saveLocation(Location location) {
+        locationRepository.save(location);
+    }
+
 }
