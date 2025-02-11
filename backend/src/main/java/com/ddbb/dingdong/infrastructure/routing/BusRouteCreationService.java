5347c91be925cf9bc546a9a6badbfad2c05f28e3
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:17:08 +0900

    [fix] Route Optimization 관련 인터페이스 제거
    #149

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/routing/BusRouteCreationService.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/routing/BusRouteCreationService.java
@@ -1,9 +1,13 @@
 package com.ddbb.dingdong.infrastructure.routing;
 
-import com.ddbb.dingdong.infrastructure.routing.api.RouteOptimizationApiClient;
 import com.ddbb.dingdong.domain.clustering.entity.Location;
-import com.ddbb.dingdong.infrastructure.routing.util.RouteOptimizationDTOConverter;
+import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
 import com.ddbb.dingdong.domain.transportation.entity.Path;
+import com.ddbb.dingdong.domain.user.entity.School;
+import com.ddbb.dingdong.infrastructure.routing.api.TmapRouteOptimizationApiClient;
+import com.ddbb.dingdong.infrastructure.routing.model.dto.RequestRouteOptimizationDTO;
+import com.ddbb.dingdong.infrastructure.routing.model.dto.ResponseRouteOptimizationDTO;
+import com.ddbb.dingdong.infrastructure.routing.util.TmapRouteOptimizationDTOConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,30 +19,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BusRouteCreationService {
 
-    private static final double SNU_LATITUDE = 37.45988;
-    private static final double SNU_LONGITUDE = 126.9519;
-    private final RouteOptimizationApiClient apiClient;
-    private final RouteOptimizationDTOConverter routeOptimizationDTOConverter;
+    private final TmapRouteOptimizationApiClient apiClient;
+    private final TmapRouteOptimizationDTOConverter routeOptimizationDTOConverter;
 
-    public Path routeOptimization(List<Location> locations) {
-        Object request, response;
+    public Path routeOptimization(List<Location> locations, School school, Direction direction) {
+        RequestRouteOptimizationDTO request;
+        ResponseRouteOptimizationDTO response;
 
-        request = generateRequests(locations);
+        request = generateRequests(locations, school, direction);
         response = apiClient.getRouteOptimization(request);
-        try {
-            Path path = routeOptimizationDTOConverter.toPath(response);
-            return path;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+
+        return routeOptimizationDTOConverter.toPath(response);
     }
 
-    private Object generateRequests(List<Location> locations) {
-        Object request;
-
-        request = routeOptimizationDTOConverter.fromLocations(locations, SNU_LATITUDE, SNU_LONGITUDE);
-
+    private RequestRouteOptimizationDTO generateRequests(List<Location> locations, School school, Direction direction) {
+        RequestRouteOptimizationDTO request;
+        request = routeOptimizationDTOConverter.fromLocations(locations, school.getLatitude(), school.getLongitude(),direction);
         return request;
     }
 }
