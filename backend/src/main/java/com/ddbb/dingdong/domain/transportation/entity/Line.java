9d7c2c6707d9621fb76108fa591a8c459c0fd557
keemjoonsung <kjs990114@naver.com>
2025-02-11 11:27:05 +0900

    [fix] 엔티티 Setter 추가
    #149

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/entity/Line.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/entity/Line.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Line {
