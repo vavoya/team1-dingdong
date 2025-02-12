d5876bd7a0e81952f8e268837678b610083fb1fd
keemjoonsung <kjs990114@naver.com>
2025-02-12 11:41:42 +0900

    [feat] 안읽은 알림이 있는지 확인 usecase 작성
    해당 유저가 읽지않은 알림이 있으면, true 없으면 false를 반환하는 usecase
    
    #154

---
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/notification/CheckUnreadNotificationUseCase.java
@@ -0,0 +1,35 @@
+package com.ddbb.dingdong.application.usecase.notification;
+
+import com.ddbb.dingdong.application.common.Params;
+import com.ddbb.dingdong.application.common.UseCase;
+import com.ddbb.dingdong.domain.notification.service.NotificationManagement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CheckUnreadNotificationUseCase implements UseCase<CheckUnreadNotificationUseCase.Param, CheckUnreadNotificationUseCase.Result> {
+    private final NotificationManagement notificationManagement;
+
+    @Override
+    public Result execute(Param param) {
+        Long userId = param.getUserId();
+        boolean hasUnreadNotifications = notificationManagement.hasUnreadNotifications(userId);
+
+        return new Result(hasUnreadNotifications);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Param implements Params {
+        private Long userId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Result {
+        boolean hasUnreadNotifications;
+    }
+}
