d341e5fc21ea70a06436d684820f7cb1a385a450
ji-woong-song <thdwldnd7@naver.com>
2025-02-12 17:29:12 +0900

    [feat]: 버스 출발 시 그때 시간 기준으로 버스 정류장 도착 예정 시간 업데이트 기능 추가
    softeer5th#134

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/BusScheduleRepository.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/BusScheduleRepository.java
@@ -2,6 +2,19 @@ package com.ddbb.dingdong.domain.transportation.repository;
 
 import com.ddbb.dingdong.domain.transportation.entity.BusSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
+@Repository
 public interface BusScheduleRepository extends JpaRepository<BusSchedule, Long> {
+    @Modifying
+    @Query("UPDATE BusStop bst SET bst.expectedArrivalTime = :newArrivalTime WHERE bst.id = :busStopId")
+    int updateBusStopArrivalTime(
+            @Param("newArrivalTime") LocalDateTime newArrivalTime,
+            @Param("busStopId") Long busStopId
+    );
 }
