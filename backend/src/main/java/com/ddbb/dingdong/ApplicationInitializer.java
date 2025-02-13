c9a3ee1fb7cc26fca637f615e8a2c542238f69f2
ji-woong-song <thdwldnd7@naver.com>
2025-02-13 14:32:56 +0900

    [fix]: School Entity에 Cascade Merge에 따른 ApplicationInitializer 수정

--- backend/src/main/java/com/ddbb/dingdong/ApplicationInitializer.java
+++ backend/src/main/java/com/ddbb/dingdong/ApplicationInitializer.java
@@ -5,6 +5,7 @@ import com.ddbb.dingdong.domain.payment.repository.WalletRepository;
 import com.ddbb.dingdong.domain.user.entity.Home;
 import com.ddbb.dingdong.domain.user.entity.School;
 import com.ddbb.dingdong.domain.user.entity.User;
+import com.ddbb.dingdong.domain.user.repository.SchoolRepository;
 import com.ddbb.dingdong.domain.user.repository.UserRepository;
 import com.ddbb.dingdong.infrastructure.auth.encrypt.PasswordEncoder;
 import jakarta.annotation.PostConstruct;
@@ -19,6 +20,7 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 @Slf4j
 public class ApplicationInitializer {
+    private final SchoolRepository schoolRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final WalletRepository walletRepository;
@@ -29,6 +31,7 @@ public class ApplicationInitializer {
             String password = passwordEncoder.encode("abcd1234!@");
             Home home = new Home(null, 37.5143, 127.0294, 37.513716, 127.029790,"에티버스");
             School school = new School(null, "seoul", "address", 1.0, 1.0);
+            school = schoolRepository.save(school);
             User user = new User(null, "test", "test@test.com", password, LocalDateTime.now(), school, null);
             user.associateHome(home);
             user = userRepository.save(user);
