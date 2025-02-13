acecde63ec93745141af33d48dc795713e91e99e
ji-woong-song <thdwldnd7@naver.com>
2025-02-13 18:29:20 +0900

    [feat]: Point, Line 테이블에서 거리, 속도, 좌표를 조회하는 기능 추가 - PathQueryRepository
    softeer5th#134

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/PathQueryRepository.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/repository/PathQueryRepository.java
@@ -2,8 +2,10 @@ package com.ddbb.dingdong.domain.transportation.repository;
 
 import com.ddbb.dingdong.domain.transportation.entity.Path;
 import com.ddbb.dingdong.domain.transportation.repository.projection.PathPointProjection;
+import com.ddbb.dingdong.domain.transportation.repository.projection.PathSegmentProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +20,15 @@ public interface PathQueryRepository extends JpaRepository<Path, Long> {
             "WHERE bs.id = :busScheduleId " +
             "ORDER BY l.sequence, pt.sequence")
     List<PathPointProjection> findPathPointsByPathId(Long busScheduleId);
+
+    @Query("SELECT " +
+            "point.latitude as latitude, point.longitude as longitude, " +
+            "line.id as lineId, line.totalMeters as meter, line.totalSeconds as second " +
+            "FROM Point point " +
+            "JOIN Line line ON line.id = point.line.id " +
+            "JOIN Path path on path.id = line.path.id " +
+            "JOIN BusSchedule bs on bs.id = path.busSchedule.id " +
+            "WHERE bs.id = :busScheduleId " +
+            "ORDER BY line.sequence, point.sequence")
+    List<PathSegmentProjection> findSegmentByBusScheduleId(@Param("busScheduleId") Long busScheduleId);
 }
