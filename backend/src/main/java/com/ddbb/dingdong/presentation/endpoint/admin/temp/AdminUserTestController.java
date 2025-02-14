65c50945bc2c3f786bb5648deecf6577779e6de3
ji-woong-song <thdwldnd7@naver.com>
2025-02-14 21:24:24 +0900

    [feat]: 정류장 위치를 설정하면서 유저를 생성하는 fast-signup API 구현
    softeer5th#197

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/admin/temp/AdminUserTestController.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/admin/temp/AdminUserTestController.java
@@ -2,11 +2,13 @@ package com.ddbb.dingdong.presentation.endpoint.admin.temp;
 
 import com.ddbb.dingdong.application.exception.APIException;
 import com.ddbb.dingdong.application.usecase.user.ChangeUserStationUseCase;
+import com.ddbb.dingdong.application.usecase.user.FastSignUpUseCase;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.domain.user.entity.User;
 import com.ddbb.dingdong.domain.user.repository.UserRepository;
 import com.ddbb.dingdong.infrastructure.auth.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.annotation.LoginUser;
+import com.ddbb.dingdong.presentation.endpoint.admin.temp.dto.FastSignUpDTO;
 import com.ddbb.dingdong.presentation.endpoint.admin.temp.dto.GetUserInfoDTO;
 import com.ddbb.dingdong.presentation.endpoint.admin.temp.dto.UserHomeUpdateDTO;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/admin/test/users")
 public class AdminUserTestController {
+    private final FastSignUpUseCase fastSignUpUseCase;
     private final ChangeUserStationUseCase changeUserStationUseCase;
     private final UserRepository userRepository;
 
@@ -56,6 +59,26 @@ public class AdminUserTestController {
         );
         try {
             changeUserStationUseCase.execute(param);
+            return null;
+        } catch (DomainException e) {
+            throw new APIException(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/fast-signup")
+    public ResponseEntity<Void> postHomeLocation(
+            @RequestBody FastSignUpDTO dto
+    ) {
+        try {
+            FastSignUpUseCase.Param param = new FastSignUpUseCase.Param(
+                    dto.getName(),
+                    dto.getEmail(),
+                    dto.getPassword(),
+                    dto.getHouseRoadNameAddress(),
+                    dto.getStationLatitude(),
+                    dto.getStationLongitude()
+            );
+            fastSignUpUseCase.execute(param);
             return null;
         } catch (DomainException e) {
             throw new APIException(e, HttpStatus.BAD_REQUEST);
