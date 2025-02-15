186ce7aa3cc21c20e8df80527318dc8156d70ed7
ji-woong-song <thdwldnd7@naver.com>
2025-02-15 16:54:25 +0900

    [feat]: 딩동 머니 잔액 조회 유스케이스 구현
    softeer5th#195

---
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/user/GetWalletBalanceUseCase.java
@@ -0,0 +1,38 @@
+package com.ddbb.dingdong.application.usecase.user;
+
+import com.ddbb.dingdong.application.common.Params;
+import com.ddbb.dingdong.application.common.UseCase;
+import com.ddbb.dingdong.domain.payment.repository.WalletProjectionRepository;
+import com.ddbb.dingdong.domain.payment.repository.projection.BalanceProjection;
+import com.ddbb.dingdong.domain.user.service.UserErrors;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GetWalletBalanceUseCase implements UseCase<GetWalletBalanceUseCase.Param, GetWalletBalanceUseCase.Response> {
+    private final WalletProjectionRepository walletProjectionRepository;
+
+    @Override
+    @Transactional
+    public Response execute(Param param) {
+        BalanceProjection projection =  walletProjectionRepository.getBalance(param.userId)
+                .orElseThrow(UserErrors.NOT_FOUND::toException);
+        return new Response(projection.getBalance());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Param implements Params {
+        Long userId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response {
+        private long balance;
+    }
+}
