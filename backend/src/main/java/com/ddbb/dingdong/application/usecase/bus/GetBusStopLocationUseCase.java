763b970a5b0e7576e14c0927ffdef2e22abbc752
ji-woong-song <thdwldnd7@naver.com>
2025-02-15 18:03:33 +0900

    [feat]: 타야할 버스 정류장 위치를 조회 유스케이스 구현
    softeer5th#196

--- backend/src/main/java/com/ddbb/dingdong/application/usecase/bus/GetBusStopLocationUseCase.java
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/bus/GetBusStopLocationUseCase.java
@@ -2,6 +2,9 @@ package com.ddbb.dingdong.application.usecase.bus;
 
 import com.ddbb.dingdong.application.common.Params;
 import com.ddbb.dingdong.application.common.UseCase;
+import com.ddbb.dingdong.domain.transportation.repository.BusScheduleQueryRepository;
+import com.ddbb.dingdong.domain.transportation.repository.projection.BusStopLocationProjection;
+import com.ddbb.dingdong.domain.transportation.service.BusErrors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +13,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class GetBusStopLocationUseCase implements UseCase<GetBusStopLocationUseCase.Param, GetBusStopLocationUseCase.Response> {
+    private final BusScheduleQueryRepository busScheduleQueryRepository;
+
     @Override
     public Response execute(Param param) {
-        return null;
+        BusStopLocationProjection busStopLocation = busScheduleQueryRepository.findBusStopLocation(param.userId, param.busScheduleId)
+                .orElseThrow(BusErrors.NO_BUS_STOP::toException);
+        return new Response(
+                busStopLocation.getLongitude(),
+                busStopLocation.getLatitude()
+        );
     }
 
     @Getter
@@ -22,6 +32,7 @@ public class GetBusStopLocationUseCase implements UseCase<GetBusStopLocationUseC
         private Long busScheduleId;
     }
 
+    @Getter
     @AllArgsConstructor
     public static class Response {
         private Double longitude;
