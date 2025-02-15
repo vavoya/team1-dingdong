82ddaf550dc841f3cfa966be865b5496e8cddd5b
admin <admin@admins-MacBook-Pro-4.local>
2025-02-15 22:58:09 +0900

    feat: 회원가입 드롭다운 학교선택 UI 추가

---
+++ src/pages/Auth/Components/Dropdown/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import {
+  ChevronIconWrapper,
+  DropdownButton,
+  DropdownContainer,
+  DropdownItem,
+  DropdownList,
+} from "./styles";
+import ChevronRightIcon from "@/components/designSystem/Icons/ChevronRightIcon";
+type SchoolType = {
+  schoolName: string;
+  schoolId: number;
+};
+interface SchoolDropdownProps {
+  schoolList: SchoolType[];
+  setSchoolId: React.Dispatch<React.SetStateAction<number>>;
+}
+export default function SchoolDropdown({
+  schoolList,
+  setSchoolId,
+}: SchoolDropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState("학교 선택");
+
+  return (
+    <DropdownContainer>
+      <DropdownButton onClick={() => setIsOpen(!isOpen)}>
+        <div>{selected}</div>
+        <ChevronIconWrapper>
+          <ChevronRightIcon />
+        </ChevronIconWrapper>
+      </DropdownButton>
+      <DropdownList $isOpen={isOpen}>
+        {schoolList.map(({ schoolName, schoolId }) => (
+          <DropdownItem
+            key={schoolName}
+            onClick={() => {
+              setSelected(schoolName);
+              setIsOpen(false);
+              setSchoolId(schoolId);
+            }}
+          >
+            {schoolName}
+          </DropdownItem>
+        ))}
+      </DropdownList>
+    </DropdownContainer>
+  );
+}
