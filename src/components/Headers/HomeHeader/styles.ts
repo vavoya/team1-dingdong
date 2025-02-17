7063c7895d4920baf3e5fa090d4206be85850fbc
vavoya <vavoya6324@gmail.com>
2025-02-17 18:38:33 +0900

    feat: 소켓 알림 구독 + 스타일 변경

--- src/components/Headers/HomeHeader/styles.ts
+++ src/components/Headers/HomeHeader/styles.ts
@@ -1,7 +1,9 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import { colors } from "@/styles/colors.ts";
 
-export const Wrapper = styled.header`
+export const Wrapper = styled.header.withConfig({
+    shouldForwardProp: (prop) => prop !== 'backgroundColor'
+})<{backgroundColor: string}>`
   position: sticky;
   top: 0;
   z-index: 1;
@@ -10,21 +12,40 @@ export const Wrapper = styled.header`
   padding: 0 20px;
   justify-content: space-between;
   align-items: center;
-  background: ${colors.gray20};
-  border-bottom: 1px solid ${colors.gray20};
+  background: ${({backgroundColor}) => backgroundColor};
+    transition: background-color 0.3s ease-in-out;
 `;
 
 export const NavList = styled.ul`
   display: flex;
 `;
 
-export const NavButton = styled.button`
-  width: 40px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const NavButton = styled.button.withConfig({
+    shouldForwardProp: prop => prop !== 'isNotification'
+})<{ isNotification?: boolean; }>`
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative; /* 가상 요소 위치 조정을 위해 필요 */
+
+    ${({ isNotification = false }) =>
+            isNotification &&
+            css`
+      &::after {
+        content: "";
+        position: absolute;
+        top: 9px;
+        right: 10px;
+        width: 7px;
+        height: 7px;
+        background-color: ${colors.orange900};
+        border-radius: 50%; /* 원형 */
+      }
+    `}
 `;
+
 
 export const Title = styled.h1`
   font-family: Happiness-Sans-Title, sans-serif;
