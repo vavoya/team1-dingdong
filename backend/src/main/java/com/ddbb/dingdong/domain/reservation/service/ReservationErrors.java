1624e73a06f1496c5d1d6681d29ecd0bc63b996d
keemjoonsung <kjs990114@naver.com>
2025-02-18 00:33:49 +0900

    [refactor] 예약 도메인 에러 상태 메세지 변경

--- backend/src/main/java/com/ddbb/dingdong/domain/reservation/service/ReservationErrors.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/reservation/service/ReservationErrors.java
@@ -10,7 +10,7 @@ public enum ReservationErrors implements ErrorInfo {
     EXPIRED_RESERVATION_DATE("해당 예약시간이 이미 마감되었습니다."),
     EXCEEDED_RESERVATION_DATE("예약 가능 기간을 초과하였습니다."),
     BEFORE_RESERVATION_DATE("예약 가능 기간이 아닙니다."),
-    NOT_SUPPORTED_RESERVATION_TIME("해당 예약 기간은 존재하지 않습니다"),
+    NOT_SUPPORTED_RESERVATION_TIME("해당 예약 시간은 지원하지 않습니다"),
 
     INVALID_BUS_TICKET("유효하지 않은 버스 티켓입니다."),
     INVALID_BUS_STOP("유효하지 않은 버스 정류장입니다."),
