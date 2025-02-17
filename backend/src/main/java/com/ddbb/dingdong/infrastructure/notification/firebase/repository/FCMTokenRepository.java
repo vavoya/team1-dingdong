3d787a238d4aec64337fb4ed61a31e9fd96f65f8
ji-woong-song <thdwldnd7@naver.com>
2025-02-17 13:57:24 +0900

    [feat]: 주어진 유저 아이디 리스트에 있으면서 active인 FCM 토큰을 조회하는 메서드 추가
    softeer5th#193

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/notification/firebase/repository/FCMTokenRepository.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/notification/firebase/repository/FCMTokenRepository.java
@@ -2,8 +2,11 @@ package com.ddbb.dingdong.infrastructure.notification.firebase.repository;
 
 import com.ddbb.dingdong.infrastructure.notification.firebase.entity.FCMToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,7 @@ public interface FCMTokenRepository extends JpaRepository<FCMToken, Long> {
     void deleteByUserIdAndToken(Long userId, String token);
 
     Optional<FCMToken> findByUserIdAndToken(Long userId, String oldToken);
+
+    @Query("SELECT fcmToken FROM FCMToken fcmToken WHERE fcmToken.userId in :userIds AND fcmToken.isActive = true")
+    List<FCMToken> findActiveTokens(@Param("userIds") List<Long> userIds);
 }
