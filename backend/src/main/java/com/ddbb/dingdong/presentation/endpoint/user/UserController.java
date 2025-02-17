53c4141b9364ba065cfac56a3d473f6c64b0597d
keemjoonsung <kjs990114@naver.com>
2025-02-18 00:26:39 +0900

    [feat] 시간표 수정/조회 controller 구현
    #17, #256

--- backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/user/UserController.java
+++ backend/src/main/java/com/ddbb/dingdong/presentation/endpoint/user/UserController.java
@@ -1,21 +1,17 @@
 package com.ddbb.dingdong.presentation.endpoint.user;
 
 import com.ddbb.dingdong.application.exception.APIException;
-import com.ddbb.dingdong.application.usecase.user.GetUserInfoUseCase;
-import com.ddbb.dingdong.application.usecase.user.GetWalletBalanceUseCase;
-import com.ddbb.dingdong.application.usecase.user.GetWalletHistoryUseCase;
+import com.ddbb.dingdong.application.usecase.user.*;
 import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.infrastructure.auth.security.AuthUser;
 import com.ddbb.dingdong.infrastructure.auth.security.annotation.LoginUser;
+import com.ddbb.dingdong.presentation.endpoint.user.exchanges.ChangeTimetableDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.ddbb.dingdong.application.usecase.user.GetUserInfoUseCase.Param;
 import static com.ddbb.dingdong.application.usecase.user.GetUserInfoUseCase.Result;
@@ -27,6 +23,8 @@ public class UserController {
     private final GetUserInfoUseCase getUserInfoUseCase;
     private final GetWalletBalanceUseCase getWalletBalanceUseCase;
     private final GetWalletHistoryUseCase getWalletHistoryUseCase;
+    private final GetTimetableUseCase getTimetableUseCase;
+    private final ChangeTimetableUseCase changeTimetableUseCase;
 
     @GetMapping("/me")
     public Result getUserInfo(
@@ -64,5 +62,49 @@ public class UserController {
         GetWalletHistoryUseCase.Result result = getWalletHistoryUseCase.execute(param);
 
         return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/timetable")
+    public ResponseEntity<GetTimetableUseCase.Result> getTimetable(
+            @LoginUser AuthUser authUser
+    ) {
+        Long userId = authUser.id();
+        GetTimetableUseCase.Param param = new GetTimetableUseCase.Param(userId);
+        GetTimetableUseCase.Result result;
+        try {
+            result = getTimetableUseCase.execute(param);
+        } catch (DomainException e) {
+            throw new APIException(e, HttpStatus.NOT_FOUND);
+        }
+
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PutMapping("/timetable")
+    public ResponseEntity<Void> putTimetable(
+            @LoginUser AuthUser authUser,
+            @RequestBody ChangeTimetableDto changeTimetableDto
+    ) {
+        Long userId = authUser.id();
+        ChangeTimetableUseCase.Param param = new ChangeTimetableUseCase.Param(
+                userId,
+                changeTimetableDto.getMonStartTime(),
+                changeTimetableDto.getMonEndTime(),
+                changeTimetableDto.getTueStartTime(),
+                changeTimetableDto.getTueEndTime(),
+                changeTimetableDto.getWedStartTime(),
+                changeTimetableDto.getWedEndTime(),
+                changeTimetableDto.getThuStartTime(),
+                changeTimetableDto.getThuEndTime(),
+                changeTimetableDto.getFriStartTime(),
+                changeTimetableDto.getFriEndTime()
+        );
+        try {
+            changeTimetableUseCase.execute(param);
+        } catch (DomainException e) {
+            throw new APIException(e, HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok().build();
     }
 }
