bf45a9d363bcb6c3b8ecdf0d3c4ca702e5575e1c
ji-woong-song <thdwldnd7@naver.com>
2025-02-17 18:02:29 +0900

    [feat]: 웹 푸시 알림을 보내는 EventListener 추가
    - 전송 실패 시, 토큰의 isActive를 false로 해야 하기에 transactional을 걸지 않음.
    - 다른 데이터를 쓰지 않기 때문에 가능
    
    softeer5th#193

---
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/notification/WebPushNotificationEventListener.java
@@ -0,0 +1,40 @@
+package com.ddbb.dingdong.infrastructure.notification;
+
+import com.ddbb.dingdong.domain.reservation.service.event.AllocationFailedEvent;
+import com.ddbb.dingdong.domain.reservation.service.event.AllocationSuccessEvent;
+import com.ddbb.dingdong.domain.transportation.service.event.BusDepartureEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class WebPushNotificationEventListener {
+    private final NotificationSender notificationSender;
+    private final NotificationMessageFormatter messageFormatter;
+
+    @Async
+    @EventListener
+    protected void sendAllocationSuccessNotification(AllocationSuccessEvent event) {
+        NotificationMessage successMessage = messageFormatter.allocateSuccess();
+        notificationSender.send(successMessage.title(), successMessage.content(), List.of(event.getUserId()));
+    }
+
+    @Async
+    @EventListener
+    protected void sendAllocationFailNotification(AllocationFailedEvent event) {
+        NotificationMessage notificationMessage = messageFormatter.allocateFail();
+        notificationSender.send(notificationMessage.title(), notificationMessage.content(), List.of(event.getUserId()));
+    }
+
+    @Async
+    @EventListener
+    protected void sendBusStartNotification(BusDepartureEvent event) {
+        NotificationMessage message = messageFormatter.busDeparture(event.getArrivalTime(), LocalDateTime.now());
+        notificationSender.send(message.title(), message.content(), event.getUserIds());
+    }
+}
