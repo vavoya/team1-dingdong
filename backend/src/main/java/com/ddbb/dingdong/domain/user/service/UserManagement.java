2fef4669f44ae23cb8adf9492e7707825c076578
keemjoonsung <kjs990114@naver.com>
2025-02-18 00:24:54 +0900

    [feat] 시간표 수정 도메인 서비스 구현
    #17

--- backend/src/main/java/com/ddbb/dingdong/domain/user/service/UserManagement.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/user/service/UserManagement.java
@@ -1,7 +1,9 @@
 package com.ddbb.dingdong.domain.user.service;
 
+import com.ddbb.dingdong.domain.user.entity.Timetable;
 import com.ddbb.dingdong.domain.user.entity.User;
 import com.ddbb.dingdong.domain.user.repository.UserRepository;
+import com.ddbb.dingdong.domain.user.service.error.UserErrors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,5 +14,10 @@ public class UserManagement {
 
     public User load(Long userId) {
         return userRepository.findById(userId).orElseThrow(UserErrors.NOT_FOUND::toException);
+    }
+
+    public void changeTimetable(Long userId, Timetable newTimetable) {
+        User user = userRepository.findById(userId).orElseThrow(UserErrors.NOT_FOUND::toException);
+        user.changeTimetable(newTimetable);
     }
 }
