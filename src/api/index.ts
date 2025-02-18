7be2c3517b9080e9dd0835418af6a06c42d97034
vavoya <vavoya6324@gmail.com>
2025-02-18 22:53:59 +0900

    feat: 환경변수

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5173", // API 서버 주소 => 서버 배포 이후 환경변수에 저장하여 변경.
+  baseURL: import.meta.env.VITE_API_BASE_URL, // API 서버 주소 => 서버 배포 이후 환경변수에 저장하여 변경.
   headers: {
     "Content-Type": "application/json",
   },
