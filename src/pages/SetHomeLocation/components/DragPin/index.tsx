01a0052d7cde7d9faa59671b49fa05fd94c77f6b
admin <admin@admins-MacBook-Pro-4.local>
2025-02-19 01:02:10 +0900

    fix: 핀 드래그로 집주소 변경 -> 지도를 움직여서 핀 위치로 주소를 변경하는 방식으로 변경. 카카오 map 과 독립적으로 동작하도록, customoverlay가 아닌 일반컴포넌트를 사용.

--- src/pages/SetHomeLocation/components/DragPin/index.tsx
+++ src/pages/SetHomeLocation/components/DragPin/index.tsx
@@ -1,12 +1,8 @@
 import PinIcon from "@/components/designSystem/Icons/PinIcon";
 import { PinContainer, PinDescription, PinMark, PinTitle } from "./styles";
 
-interface DragPinProps {
-  onMouseDown: (e: React.MouseEvent<HTMLDivElement>) => void;
-}
-
-export const DragPin = ({ onMouseDown }: DragPinProps) => (
-  <PinContainer onMouseDown={onMouseDown}>
+export const DragPin = () => (
+  <PinContainer>
     <PinMark>
       <PinTitle>탑승지</PinTitle>
       <PinDescription>정확한 승하차 위치를 설정해주세요.</PinDescription>
