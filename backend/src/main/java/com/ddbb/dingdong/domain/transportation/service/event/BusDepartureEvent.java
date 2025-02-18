d02ddf3cb47534a9f6e9f073440bd20db1fe1b10
keemjoonsung <kjs990114@naver.com>
2025-02-18 17:50:59 +0900

    [fix] 버스 출발 event 수정
    List<busStopId> 만 전달하도록 수정

--- backend/src/main/java/com/ddbb/dingdong/domain/transportation/service/event/BusDepartureEvent.java
+++ backend/src/main/java/com/ddbb/dingdong/domain/transportation/service/event/BusDepartureEvent.java
@@ -3,12 +3,10 @@ package com.ddbb.dingdong.domain.transportation.service.event;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class BusDepartureEvent {
-    private LocalDateTime arrivalTime;
-    private List<Long> userIds;
+    private List<Long> busStopIds;
 }
