6a832ae4d1855ba5fd3d4d928f7473c4daa89adb
admin <admin@admins-MacBook-Pro-4.local>
2025-02-19 00:57:04 +0900

    fix: AI추천 적용 -> 시간표 추천 적용 변경에 따라, 함수 또는 컴포넌트명 변경

--- src/api/BusBooking/customBusBooking.ts
+++ src/api/BusBooking/customBusBooking.ts
@@ -1,13 +1,11 @@
 import { axiosInstance } from "@/api";
 
-export async function getAIRecommendationTime(year: number, month: number) {
+export async function getTimeTableRecommendationTime() {
   try {
-    const { data } = await axiosInstance.get(
-      `/api/users/reservations/recommendtaions?status=go&year=${year}&month=${month}`
-    );
+    const { data } = await axiosInstance.get("/api/users/timetable");
     return data;
   } catch (err: any) {
-    console.error("AI 추천 적용 GET 요청 실패:", err);
+    console.error("시간표 추천 적용 GET 요청 실패:", err);
     throw new Error(err);
   }
 }
