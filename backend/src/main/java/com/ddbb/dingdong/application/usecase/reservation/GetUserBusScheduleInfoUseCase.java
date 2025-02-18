c12e9da0fea292fc213bba3e272528dc2c9a5f11
keemjoonsung <kjs990114@naver.com>
2025-02-18 15:01:55 +0900

    [feat] 유저 버스 배차 조회 usecase
    query를 그대로 호출
    
    #264

---
+++ backend/src/main/java/com/ddbb/dingdong/application/usecase/reservation/GetUserBusScheduleInfoUseCase.java
@@ -0,0 +1,72 @@
+package com.ddbb.dingdong.application.usecase.reservation;
+
+import com.ddbb.dingdong.application.common.Params;
+import com.ddbb.dingdong.application.common.UseCase;
+import com.ddbb.dingdong.domain.reservation.entity.vo.Direction;
+import com.ddbb.dingdong.domain.reservation.repository.ReservationQueryRepository;
+import com.ddbb.dingdong.domain.reservation.repository.projection.UserReservationProjection;
+import com.ddbb.dingdong.domain.reservation.service.ReservationErrors;
+import com.ddbb.dingdong.domain.transportation.entity.vo.OperationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class GetUserBusScheduleInfoUseCase implements UseCase<GetUserBusScheduleInfoUseCase.Param, GetUserBusScheduleInfoUseCase.Result> {
+    private final ReservationQueryRepository reservationQueryRepository;
+
+    @Override
+    public Result execute(Param param) {
+        return getUserBusScheduleInfo(param);
+    }
+
+    private Result getUserBusScheduleInfo(Param param) {
+        Long userId = param.getUserId();
+        Long busScheduleId = param.getBusScheduleId();
+        UserReservationProjection projection = reservationQueryRepository.queryReservationByBusScheduleIdAndUserId(userId, busScheduleId)
+                .orElseThrow(ReservationErrors.BUS_SCHEDULE_NOT_FOUND::toException);
+
+        return new Result(
+                projection.getReservationId(),
+                projection.getBusStopRoadNameAddress(),
+                projection.getDirection(),
+                projection.getExpectedArrivalTime(),
+                projection.getExpectedDepartureTime(),
+                new Result.OperationInfo(
+                        projection.getBusStatus(),
+                        projection.getBusName(),
+                        projection.getBusStopArrivalTime()
+                )
+        );
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Param implements Params {
+        private Long userId;
+        private Long busScheduleId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Result {
+        private Long reservationId;
+        private String busStopName;
+        private Direction direction;
+        private LocalDateTime expectedArrivalTime;
+        private LocalDateTime expectedDepartureTime;
+        private OperationInfo operationInfo;
+
+        @Getter
+        @AllArgsConstructor
+        public static class OperationInfo {
+            private OperationStatus busStatus;
+            private String busName;
+            private LocalDateTime busStopArrivalTime;
+        }
+    }
+}
