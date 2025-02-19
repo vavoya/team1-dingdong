6d06c5b99ddf06d95b429dbc4ade266a473d2e81
vavoya <vavoya6324@gmail.com>
2025-02-19 19:46:47 +0900

    feat: consle.log 제거

--- src/pages/Reservations/page.tsx
+++ src/pages/Reservations/page.tsx
@@ -10,13 +10,12 @@ export default function Page() {
     const [allReservations, allocatedReservations, pendingReservations, endedReservations, canceledReservations]: users_reservations_interface[]  = useLoaderData();
 
 
-    console.log(allocatedReservations.reservationInfos.content)
     return (
         <PageWrapper>
             <PopHeader text={"예매 내역"} />
             <PageMain>
                 {/* 여기 슬라이드 구현 */}
-                <CardSlider contents={allocatedReservations.reservationInfos.content as Extract< users_reservations_interface['reservationInfos']['content'][number], TO_SCHOOL_ALLOCATED | TO_HOME_ALLOCATED>[]} />
+                <CardSlider contents={allocatedReservations.reservationInfos.content as Extract<users_reservations_interface['reservationInfos']['content'][number], TO_SCHOOL_ALLOCATED | TO_HOME_ALLOCATED>[]} />
                 <BookingHistory
                     allReservations={allReservations}
                     allocatedReservations={allocatedReservations}
