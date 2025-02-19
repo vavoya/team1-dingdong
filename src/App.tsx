c483c04ed097b7355e7d1144aee0a28cba424c8e
admin <admin@admins-MacBook-Pro-4.local>
2025-02-19 15:48:58 +0900

    merge: frontend-dev PR 새로 반영 완료

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,21 @@
 import ResetStyle from "@/styles/ResetStyle.ts";
 import GlobalStyle from "./styles/GlobalStyle.ts";
 import "@/webPushNotification/settingFCM.ts";
-import { handleAllowNotification } from "./webPushNotification/handleAllowNotification.ts";
-import {lazy, Suspense, useEffect} from "react";
+import { lazy, Suspense } from "react";
 import LoadingModal from "@/components/Loading";
 
 // `RouterProvider` 자체를 동적 import
 const LazyRouterProvider = lazy(async () => {
-  const [{RouterProvider}, {router}] = await Promise.all([
+  const [{ RouterProvider }, { router }] = await Promise.all([
     import("react-router-dom"),
-    import("@/route")
-  ])
+    import("@/route"),
+  ]);
   return {
-    default: () => <RouterProvider router={router} />
-  }
-})
-
+    default: () => <RouterProvider router={router} />,
+  };
+});
 
 function App() {
-  const notificationPermission = async () => {
-    try {
-      const notificationResult = await handleAllowNotification();
-      // default : 사용자가 아직 해당 서비스에 대한 알림을 권한을 선택 안한 경우
-      // default 일 때, 모달을 띄어서 허용 여부?
-      if (notificationResult === "granted") {
-        document.body.style.overflowY = "scroll"; // granted의 로딩 후에 실행
-      }
-    } finally {
-    }
-  };
-  // 푸시알림 허용 창 띄우기 로직
-  useEffect(() => {
-    // if 로그인을 한 유저 일 때.
-    notificationPermission();
-  }, []);
   return (
     <>
       <ResetStyle />
