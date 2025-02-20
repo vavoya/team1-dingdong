960e1b6a829dac993c76416625a50ef0096102f2
admin <admin@admins-MacBook-Pro-4.local>
2025-02-20 17:57:51 +0900

    fix: 탑승지 위치 변경 설정시, 주소가 실시간으로 바뀌지 않음

--- src/pages/SetHomeLocation/components/MapWrapper/index.tsx
+++ src/pages/SetHomeLocation/components/MapWrapper/index.tsx
@@ -1,4 +1,3 @@
-
 import { useRef, useState } from "react";
 
 import {
@@ -37,7 +36,6 @@ export default function SetLocationHomeMap({
   useKakaoLoader();
   const mapRef = useRef<kakao.maps.Map>();
 
-
   const [houseAndStationInfo] = useLoaderData();
 
   const { stationInfo: serverStationInfo } = houseAndStationInfo;
@@ -47,22 +45,20 @@ export default function SetLocationHomeMap({
   });
   useAddress(centerPosition, setRoadAddress);
 
-  const currentStationPosition = {
-    lat: serverStationInfo.latitude,
-    lng: serverStationInfo.longitude,
-  };
-
   // 지도 중심 좌표가 변경될 때 호출되는 함수
   const handleCenterChanged = () => {
     if (!mapRef.current) return;
 
     const center = mapRef.current.getCenter();
 
-    setCenterPosition(currentStationPosition);
+    console.log(center.getLat(), center.getLng(), "좌표 좌푠");
+    setCenterPosition({
+      lat: center.getLat(),
+      lng: center.getLng(),
+    });
     setStationInfo({
       latitude: center.getLat(),
       longitude: center.getLng(),
-
     });
     setShowBottomSheet(true);
   };
