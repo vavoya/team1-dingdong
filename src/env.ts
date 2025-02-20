1566cdc580803a276a01f1b253f68bf54eba4366
vavoya <vavoya6324@gmail.com>
2025-02-20 14:54:16 +0900

    feat: 상수 추가

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,9 @@
 
 
 export const unitPrice = 1000
+export const FREE_CHARGE_PRICE = 1000
 
-export const kakaoAppKey = '52b52f6bf1759dc818657372f52a7d2f'
+
+// 예매 API 초기
+export const INIT_PAGE_SIZE = 10
+export const INIT_PAGE = 0
