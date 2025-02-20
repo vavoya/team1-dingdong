47cb693b3fec054bc5f8ec62ca60b57cbebca95f
ji-woong-song <thdwldnd7@naver.com>
2025-02-20 14:47:31 +0900

    [feat]: 다른 스레드를 Busy Waiting 하게 기다리지 않고, 시그널 기반으로 기다리게 개션
    쓸데없이 조건식을 체크하러 깨어나지 않아도 됨.
    무한정 기다리지 않게 하기 않게 하기 위해 waitingCount를 추가했고, 정확한 개수는 아니더라도 한번 > 0 임을 판단하면 중간 과정에서 waitingCount가 줄어드는 일이 없도록 보장함.
    
    softeer5th#304

--- backend/src/main/java/com/ddbb/dingdong/infrastructure/bus/simulator/subscription/StoppableLock.java
+++ backend/src/main/java/com/ddbb/dingdong/infrastructure/bus/simulator/subscription/StoppableLock.java
@@ -4,29 +4,31 @@ import com.ddbb.dingdong.domain.common.exception.DomainException;
 import com.ddbb.dingdong.domain.transportation.service.BusErrors;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
 public class StoppableLock {
-    private static final long BUSY_WAIT_INTERVAL_MILLIS = 100L;
     private final ReentrantLock lock = new ReentrantLock(true);
-    private final AtomicBoolean isStopped = new AtomicBoolean(false);
+    private final Condition terminateCondition = lock.newCondition();
+    private final AtomicInteger waitingCount = new AtomicInteger(0);
+    private volatile boolean isStopped = false;
 
     /**
+     * isStopped을 읽기만 합니다.
      * 이미 stop된 상태라면 바로 false를 반환하고,
      * lock을 얻고 stop 된 상태라면 바로 lock을 반납하고 false를 반환합니다.
      * true를 반환하면 외부에서 반드시 unlock을 해야합니다.
      * **/
     public boolean lock() {
-        if (isStopped.get()) {
+        if (isStopped) {
             return false;
         }
+        waitingCount.incrementAndGet();
         lock.lock();
-        if (isStopped.get()) {
-            lock.unlock();
+        if (isStopped) {
+            this.unlock();
             return false;
         }
         return true;
@@ -34,24 +36,43 @@ public class StoppableLock {
 
     public void unlock() {
         if (lock.isHeldByCurrentThread()) {
+            int count = this.waitingCount.decrementAndGet();
+            if (count == 0) {
+                terminateCondition.signal();
+            }
             lock.unlock();
         }
     }
 
+    /**
+     * isStopped 의 값은 lock을 얻었을 때 True로 변화시킬 수 있고,
+     * waitingCount 값은 lock을 얻었을 때 decrease 시킬 수 있고, 얻기 전엔 increase만 가능하다
+     * 따라서 stop 스레드가 해당 락에 대기 중인 개수를 세고 나서 waiting count가 줄어들 수 없다. (항상 lock을 얻어야 하기 때문)
+     * 따라서 stopAndWait 함수에서 적어도 실제 대기 중인 스레드가 없는데 기다리는 경우는 발생하지 않는다.
+     * **/
     public void stopAndWait() {
-        isStopped.set(true);
-        while (lock.hasQueuedThreads()) {
-            try {
-                TimeUnit.MILLISECONDS.sleep(BUSY_WAIT_INTERVAL_MILLIS);
-            } catch (InterruptedException e) {
+        lock.lock();
+        try {
+            isStopped = true;
+            while (waitingCount.get() > 0) {
+                terminateCondition.await();
+            }
+        }
+        catch (Exception e) {
+            if (e instanceof InterruptedException) {
                 Thread.currentThread().interrupt();
-                log.debug(e.getMessage());
-                throw new DomainException(BusErrors.STOP_BUS_ERROR);
+            }
+            log.debug(e.getMessage());
+            throw new DomainException(BusErrors.STOP_BUS_ERROR);
+        }
+        finally {
+            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
+                lock.unlock();
             }
         }
     }
 
     public boolean isStopped() {
-        return isStopped.get();
+        return isStopped;
     }
 }
