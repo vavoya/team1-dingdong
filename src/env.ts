08287ab66e1a3c0cef846301f790e7b80177ac0a
vavoya <vavoya6324@gmail.com>
2025-02-21 11:20:01 +0900

    feat: env.ts 상수 값 추가

--- src/env.ts
+++ src/env.ts
@@ -7,3 +7,7 @@ export const FREE_CHARGE_PRICE = 1000
 // 예매 API 초기
 export const INIT_PAGE_SIZE = 10
 export const INIT_PAGE = 0
+
+// wallet 조회 초기
+export const INIT_WALLET_PAGE_SIZE = 20
+export const INIT_WALLET_PAGE = 0
