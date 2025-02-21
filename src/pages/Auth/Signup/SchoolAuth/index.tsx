29ff01dc3dd0937af92802a958dbb9ad14cab5fa
admin <admin@admins-MacBook-Pro-4.local>
2025-02-21 12:34:34 +0900

    design: Auth 페이지 하단 버튼 type sumbit으로 변경 - 로그인 페이지에서 학교이메일 -> 이메일로 변경 - 로그인 하단 버튼 "다음" -> "로그인" 으로 텍스트 변경

--- src/pages/Auth/Signup/SchoolAuth/index.tsx
+++ src/pages/Auth/Signup/SchoolAuth/index.tsx
@@ -127,6 +127,7 @@ export default function SchoolAuthSignUp() {
 
       <NextButtonWrapper>
         <SolidButton
+          type="submit"
           text="다음"
           onClick={checkVerificationCodeHandler}
           active={nextButtonActive}
