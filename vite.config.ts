15d1cb5eeb09533da05b6032ecc3abefdbee76e6
admin <admin@admins-MacBook-Pro-4.local>
2025-02-23 02:30:43 +0900

    build: manualChunks를 이용하여 청크를 쪼개어 번들 사이즈를 줄임.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
+
   resolve: {
     alias: [
       { find: "@", replacement: "/src" }, // `@`을 `src` 폴더로 매핑
