c52732304f5c875d4900367983d0725b89fe3fde
admin <admin@admins-MacBook-Pro-4.local>
2025-02-22 22:42:51 +0900

    design: 모달을 띄울 때 글자수가 길어지니 UI가 좋지 않아서 줄바꿈 문자도 표현 되도록 속성 추가

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -5,55 +5,56 @@ import {CTAButtonB} from "@/styles/buttons.ts";
 
 
 export const Backdrop = styled.div`
-    position: fixed;
-    z-index: 9999;
+  position: fixed;
+  z-index: 9999;
     top:0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
     background: rgba(0, 0, 0, 0.50);
 `
 
 export const ModalWrapper = styled.div`
-    width: 315px;
+  width: 315px;
 
-    border-radius: 12px;
-    background: ${colors.gray0};
+  border-radius: 12px;
+  background: ${colors.gray0};
 `
 
 export const ModalInfo = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    width: 100%;
-    padding: 32px 20px;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
+  padding: 32px 20px;
+  text-align: center;
 `
 
 export const ModalTitle = styled(Heading1Bold)`
-    color: ${colors.gray100};
-`
+  color: ${colors.gray100};
+  white-space: break-spaces;
+`;
 
 export const ModalText = styled(Body1Regular)`
-    color: ${colors.gray70};
+  color: ${colors.gray70};
 `
 
 export const ButtonBox = styled.div`
-    display: flex;
-    width: 100%;
-    padding: 0 20px 20px;
-    justify-content: space-between;
-    gap: 20px;
+  display: flex;
+  width: 100%;
+  padding: 0 20px 20px;
+  justify-content: space-between;
+  gap: 20px;
 `
 
 export const Button = styled(CTAButtonB)`
-    flex: 1;
+  flex: 1;
 `
 
 export const ButtonText = styled(Body1Regular)`
-    color: ${colors.gray100};
+  color: ${colors.gray100};
 `
