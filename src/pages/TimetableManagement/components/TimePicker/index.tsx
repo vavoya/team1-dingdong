f9a4ef799021f8b64ca3ec980931e1fb90d0beb3
admin <admin@admins-MacBook-Pro-4.local>
2025-02-23 00:16:48 +0900

    style: 데모데이 때 피드백 반영, 오후 12시 -> 24:00로 바뀌는 버그 수정

--- src/pages/TimetableManagement/components/TimePicker/index.tsx
+++ src/pages/TimetableManagement/components/TimePicker/index.tsx
@@ -1,61 +1,68 @@
 import TimePickerWheel, {TimeWheelHandle} from "@/components/TimePickerWheel";
 import {
-    Backdrop,
-    ButtonWrapper,
+  Backdrop,
+  ButtonWrapper,
     LeftButton, LeftText,
     Modal, RightButton, RightText,
     Title
 } from "@/pages/TimetableManagement/components/TimePicker/styles.ts";
-import {Dispatch, SetStateAction, useEffect, useRef, useState} from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 interface TimePickerProps {
     unmountModal: () => void
-    timeState: string;
-    setTimeState: Dispatch<SetStateAction<string>>;
+  timeState: string;
+  setTimeState: Dispatch<SetStateAction<string>>;
 }
 export default function TimePicker({unmountModal, timeState, setTimeState}: TimePickerProps) {
-    const [isMount, setIsMount] = useState(false);
+  const [isMount, setIsMount] = useState(false);
     const timeWheelRef = useRef<TimeWheelHandle>(null)
 
-    useEffect(() => {
-        setIsMount(true);
-    }, []);
+  useEffect(() => {
+    setIsMount(true);
+  }, []);
 
-    const unmountModalTimer = () => {
-        if (timeWheelRef.current) {
-            const {amPm, hour, minute} = timeWheelRef.current.getSelectedTime()
-            const hour24 = (amPm === '오후' ? +hour + 12 : +hour).toString().padStart(2, '0');
-            setTimeState(`${hour24}:${minute}`)
-        }
-        setIsMount(false);
-        setTimeout(() => unmountModal(), 200);
+  const unmountModalTimer = () => {
+    if (timeWheelRef.current) {
+      const { amPm, hour, minute } = timeWheelRef.current.getSelectedTime();
+      const hour24 = (
+        amPm === "오후" && +hour !== 12
+          ? +hour + 12
+          : amPm === "오전" && +hour === 12
+          ? 0
+          : +hour
+      )
+        .toString()
+        .padStart(2, "0");
+      setTimeState(`${hour24}:${minute}`);
     }
+    setIsMount(false);
+    setTimeout(() => unmountModal(), 200);
+  };
 
-
-    return (
-        <Backdrop>
-            <Modal isMount={isMount}>
+  return (
+    <Backdrop>
+      <Modal isMount={isMount}>
                 <Title>
                     시간을 선택하세요.
                 </Title>
             <TimePickerWheel ref={timeWheelRef} initTime={timeState !== '-' ? {initHour: Number(timeState.split(':')[0]) - 1, initMinute: Number(timeState.split(':')[1]) === 30 ? 1 : 0} : undefined}/>
-            <ButtonWrapper>
+        <ButtonWrapper>
                 <LeftButton onClick={() => {
                     unmountModalTimer()
                 }}>
                     <LeftText>
                         취소
                     </LeftText>
-                </LeftButton>
+          </LeftButton>
                 <RightButton onClick={() => {
                     unmountModalTimer()
                 }}>
                     <RightText>
                         확인
                     </RightText>
-                </RightButton>
-            </ButtonWrapper>
-            </Modal>
-        </Backdrop>
+          </RightButton>
+        </ButtonWrapper>
+      </Modal>
+    </Backdrop>
     )
 }
