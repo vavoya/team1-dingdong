a4bf8cd30fa1d8c7cb19e940b19dd7740f4e82b9
vavoya <vavoya6324@gmail.com>
2025-02-24 17:19:42 +0900

    refactor: frontend 폴더로 이동

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,9 +7,7 @@ import App from "./App.tsx";
 export const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
 );
