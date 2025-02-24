b62b0092f45a7da500560845feec7e76035ad513
admin <admin@admins-MacBook-Pro-4.local>
2025-02-24 17:00:12 +0900

    fix: PTR (당겨서 새로고침 기능 없애는 걸로 결정). 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+// 전역 상태
 import ResetStyle from "@/styles/ResetStyle.ts";
 import GlobalStyle from "./styles/GlobalStyle.ts";
 import "@/webPushNotification/settingFCM.ts";
-import { lazy, Suspense, useRef } from "react";
+import { lazy, Suspense } from "react";
 import LoadingModal from "@/components/Loading";
-import { PullToRefreshContainer } from "./components/PullToRefresh/PullToRefresh.tsx";
 
 // `RouterProvider` 자체를 동적 import
 const LazyRouterProvider = lazy(async () => {
@@ -17,18 +17,14 @@ const LazyRouterProvider = lazy(async () => {
 });
 
 function App() {
-  const appRef = useRef<HTMLDivElement>(null);
-
   return (
-    <PullToRefreshContainer el={appRef}>
-      <div ref={appRef}>
-        <ResetStyle />
-        <GlobalStyle />
-        <Suspense fallback={<LoadingModal text={"페이지 불러오는 중"} />}>
-          <LazyRouterProvider />
-        </Suspense>
-      </div>
-    </PullToRefreshContainer>
+    <>
+      <ResetStyle />
+      <GlobalStyle />
+      <Suspense fallback={<LoadingModal text={"페이지 불러오는 중"} />}>
+        <LazyRouterProvider />
+      </Suspense>
+    </>
   );
 }
 
